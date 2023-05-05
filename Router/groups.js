9276462d72a14928b4217d7351ab953ace09bffd
BillGray <junjason1126@gmail.com>
2023-05-05 15:16:26 -0700

    creating group db ver1

--- Router/groups.js
+++ Router/groups.js
@@ -2,7 +2,6 @@ const express = require("express");
 const Router = express.Router();
 const fs = require("fs");
 const pool = require('../model/pool');
-const axios = require("axios")
 
 Router.get("/", async (req, res) => {
   if(req.session.loggedin == true){
@@ -13,6 +12,29 @@ Router.get("/", async (req, res) => {
 })
 
 Router.get('/create', (req, res) => {
+  if(req.session.loggedin == true) {
+    res.render('group/create', {loggedin: true});
+  } else {
+    res.render('group/create', {loggedin: false});
+  }
+})
+
+Router.post('/create-validate', async(req, res) => {
+  /* if(!req.session.loggedin){
+    return res.redirect("/account");
+  } */
+  const connection = await (await pool).getConnection();
+  console.log(req.body)
+  /* var group = {
+    name: req.body,
+    email: email,
+    hashed_password: hashed[1],
+    salt: hashed[0],
+  } */
+  //connection.query('INSERT INTO groups SET ?', user);
+})
+
+Router.get('/join', (req, res) => {
   if(req.session.loggedin == true) {
     res.render('group/create', {loggedin: true});
   } else {
