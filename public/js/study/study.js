17d5a89ca2e1f9872564b21520972b1ae6d584aa
BillGray <junjason1126@gmail.com>
2023-05-08 16:30:58 -0700

    drag drop ver 1

--- public/js/study/study.js
+++ public/js/study/study.js
@@ -1,9 +1,12 @@
 function createSubjects(subject_number, subject, subjectColor){
   const div = document.createElement("div");
-  div.setAttribute("class", "SW d-1");
+  div.setAttribute("class", "SW d-1 list-item");
   div.setAttribute("id", "SW"+subject_number);
   div.setAttribute("draggable", "true");
   div.innerHTML=`<div id="disp">
+  <div class="item-content">
+  <span class="order">1</span>
+</div>
   <div id="digits">
     <span id="hr${subject_number}">00:</span>
     <span id="min${subject_number}">00:</span>
@@ -17,7 +20,8 @@ function createSubjects(subject_number, subject, subjectColor){
   <div id="playBtn${subject_number}" class="button" style = "background: ${subjectColor}">
     <span class="material-symbols-outlined">play_arrow</span>
   </div>
-</div>`
+</div>
+`
 const timerContainer = document.querySelector(".timer .container");
 timerContainer.appendChild(div);
 }
@@ -63,6 +67,104 @@ var timers = [];
       }
     })(i));
   }
+
+
+  console.log(document.querySelectorAll(".SW.d-1.list-item")[0]);
+  var rowSize = document.querySelector(".timer .container").clientHeight / document.querySelectorAll('.SW')[0].clientHeight; // => container height / number of items
+  var container = document.querySelector(".timer .container");
+  var listItems = Array.from(document.querySelectorAll(".list-item")); // Array of elements
+  var sortables = listItems.map(Sortable); // Array of sortables
+  var total = sortables.length;
+  console.log(rowSize, Draggable);
+  container.style.opacity = 1;
+  
+  function changeIndex(item, to) {
+    // Change position in array
+    arrayMove(sortables, item.index, to);
+  
+    // Change element's position in DOM. Not always necessary. Just showing how.
+    if (to === total - 1) {
+      container.appendChild(item.element);
+    } else {
+      var i = item.index > to ? to : to + 1;
+      container.insertBefore(item.element, container.children[i]);
+    }
+  
+    // Set index for each sortable
+    sortables.forEach(function (sortable, index) {
+      return sortable.setIndex(index);
+    });
+  }
+  
+  function Sortable(element, index) {
+    var content = element.querySelector(".item-content");
+    var order = element.querySelector(".order");
+  
+    var animation = content.style.boxShadow = "rgba(0,0,0,0.2) 0px 16px 32px 0px";
+    content.style.transform = "scale(1.1)";
+    var dragger = new Draggable.Draggable(element, {
+      onDragStart: downAction,
+      onRelease: upAction,
+      onDrag: dragAction,
+      cursor: "inherit",
+      type: "y"
+    });
+  
+    // Public properties and methods
+    var sortable = {
+      dragger: dragger,
+      element: element,
+      index: index,
+      setIndex: setIndex
+    };
+  
+    element.style.transform = "translateY(" + index * rowSize + "px)";
+    order.textContent = index + 1;
+  
+    function setIndex(index) {
+      sortable.index = index;
+      order.textContent = index + 1;
+  
+      // Don't layout if you're dragging
+      if (!dragger.isDragging) layout();
+    }
+  
+    function downAction() {
+      animation = content.style.boxShadow = "rgba(0,0,0,0.2) 0px 16px 32px 0px";
+      this.update();
+    }
+  
+    function dragAction() {
+      // Calculate the current index based on element's position
+      var index = clamp(Math.round(this.y / rowSize), 0, total - 1);
+  
+      if (index !== sortable.index) {
+        changeIndex(sortable, index);
+      }
+    }
+  
+    function upAction() {
+      animation = content.style.boxShadow = "none";
+      content.style.transform = "scale(1)";
+      layout();
+    }
+  
+    function layout() {
+      element.style.transform = "translateY(" + sortable.index * rowSize + "px)";
+    }
+  
+    return sortable;
+  }
+  
+  // Changes an elements's position in array
+  function arrayMove(array, from, to) {
+    array.splice(to, 0, array.splice(from, 1)[0]);
+  }
+  
+  // Clamps a value to a min/max
+  function clamp(value, a, b) {
+    return value < a ? a : value > b ? b : value;
+  }  
 })();
 
 
@@ -202,105 +304,3 @@ addSubjectSubmitBtn.addEventListener('click', () => {
   recommendedColorsIndex += 1;
   color.value = recommendedColors[recommendedColorsIndex];
 })
-
-
-var rowSize = 100; // => container height / number of items
-var container = document.querySelector(".timer .container");
-var listItems = Array.from(document.querySelectorAll(".SW")); // Array of elements
-var sortables = listItems.map(Sortable); // Array of sortables
-var total = sortables.length;
-
-container.style.opacity = 1;
-
-function changeIndex(item, to) {
-  // Change position in array
-  arrayMove(sortables, item.index, to);
-
-  // Change element's position in DOM. Not always necessary. Just showing how.
-  if (to === total - 1) {
-    container.appendChild(item.element);
-  } else {
-    var i = item.index > to ? to : to + 1;
-    container.insertBefore(item.element, container.children[i]);
-  }
-
-  // Set index for each sortable
-  sortables.forEach((sortable, index) => sortable.setIndex(index));
-}
-
-function Sortable(element, index) {
-  var content = element.querySelector(".item-content");
-  var order = element.querySelector(".order");
-
-  var animation = {
-    play: function() {
-      content.style.boxShadow = "rgba(0,0,0,0.2) 0px 16px 32px 0px";
-      content.style.transform = "scale(1.1)";
-    },
-    reverse: function() {
-      content.style.boxShadow = "";
-      content.style.transform = "";
-    }
-  };
-
-  var dragger = new Draggable(element, {
-    onDragStart: downAction,
-    onRelease: upAction,
-    onDrag: dragAction,
-    cursor: "inherit",
-    type: "y"
-  });
-
-  // Public properties and methods
-  var sortable = {
-    dragger: dragger,
-    element: element,
-    index: index,
-    setIndex: setIndex
-  };
-
-  element.style.transform = "translateY(" + index * rowSize + "px)";
-
-  function setIndex(index) {
-    sortable.index = index;
-    order.textContent = index + 1;
-
-    // Don't layout if you're dragging
-    if (!dragger.isDragging) layout();
-  }
-
-  function downAction() {
-    animation.play();
-    this.update();
-  }
-
-  function dragAction() {
-    // Calculate the current index based on element's position
-    var index = clamp(Math.round(this.y / rowSize), 0, total - 1);
-
-    if (index !== sortable.index) {
-      changeIndex(sortable, index);
-    }
-  }
-
-  function upAction() {
-    animation.reverse();
-    layout();
-  }
-
-  function layout() {
-    element.style.transform = "translateY(" + sortable.index * rowSize + "px)";
-  }
-
-  return sortable;
-}
-
-// Changes an elements's position in array
-function arrayMove(array, from, to) {
-  array.splice(to, 0, array.splice(from, 1)[0]);
-}
-
-// Clamps a value to a min/max
-function clamp(value, a, b) {
-  return value < a ? a : value > b ? b : value;
-}
