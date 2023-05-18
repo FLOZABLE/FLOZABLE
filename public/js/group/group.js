039dee66ccaf15a418d5442ec47759018e04fb01
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2023-05-18 05:25:03 -0700

    my group area

--- public/js/group/group.js
+++ public/js/group/group.js
@@ -1,5 +1,20 @@
 const groupsWrapper = document.querySelector(".groups-container");
 const pwAskingModal = document.querySelector(".pw-modal");
+const mygroupsWrapper = document.querySelector(".mygroup-container");
+const closeBtn = document.querySelectorAll(".close-btn");
+const modal = document.querySelector('.subject-modal');
+
+const DelayModalEvent = () => {
+  modal.style = 'display: block';
+}
+
+closeBtn[0].addEventListener('click', () => {
+  modal.style =  "display: none";
+});
+
+closeBtn[1].addEventListener('click', () => {
+  pwAskingModal.style =  "display: none";
+});
 
 function changeBrightness(hex, percent) {
   // remove the # symbol if it's present
@@ -21,63 +36,78 @@ function changeBrightness(hex, percent) {
   return newHex;
 }
 
-const joinButtonEvent = async(joinLeaveButton, groupId) => {
-  let response = await fetch(`/groups/join/${groupId}`, {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json'
+const joinButtonEvent = async(joinLeaveButton, groupId, type) => {
+  if(type == 'public') {
+    let response = await fetch(`/groups/join/${groupId}`, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    response = await response.json();
+  
+    if(response.success == true) {
+      joinLeaveButton.innerHTML = `
+      LEAVE GROUP
+    <span class="blob-btn__inner">
+      <span class="blob-btn__blobs">
+        <span class="blob-btn__blob"></span>
+        <span class="blob-btn__blob"></span>
+        <span class="blob-btn__blob"></span>
+        <span class="blob-btn__blob"></span>
+      </span>
+      `;
+      joinLeaveButton.removeEventListener('click', () => joinButtonEvent(joinLeaveButton, group.group_id, type));
+      joinLeaveButton.addEventListener('click', () => DelayModalEvent());
+      setTimeout(() => {
+        joinLeaveButton.addEventListener('click', () => leaveButtonEvent(joinLeaveButton, groupId, type));
+        joinLeaveButton.removeEventListener('click', () => DelayModalEvent());
+      }, 1000 * 60 * 10);
     }
-  });
-  response = await response.json();
-
-  if(response.success == true) {
-    joinLeaveButton.innerHTML = `
-    LEAVE GROUP
-  <span class="blob-btn__inner">
-    <span class="blob-btn__blobs">
-      <span class="blob-btn__blob"></span>
-      <span class="blob-btn__blob"></span>
-      <span class="blob-btn__blob"></span>
-      <span class="blob-btn__blob"></span>
-    </span>
-    `;
-    joinLeaveButton.removeEventListener('click', () => joinButtonEvent(joinLeaveButton, group.group_id));
-    joinLeaveButton.addEventListener('click', () => DelayModalEvent());
-    setTimeout(() => {
-      joinLeaveButton.addEventListener('click', () => leaveButtonEvent(joinLeaveButton, groupId));
-      joinLeaveButton.removeEventListener('click', () => DelayModalEvent());
-    }, 1000 * 60 * 10);
-  } else if(response.reason == 'pw_required'){
+  } else {
     pwAskingModal.style = 'display: block';
-    const groupPw = pwAskingModal.querySelector("input").value;
+    const groupPw = pwAskingModal.querySelector("input");
     const groupPwSubmitBtn = pwAskingModal.querySelector("button#join-pw");
-    /* groupPwSubmitBtn.addEventListener('click', () => {
-      (async() => {
-        const response = await fetch('/') => {
-          
-        }
-      })
-    }) */
+    groupPwSubmitBtn.addEventListener('click', async() => {
+      console.log(groupPw.value)
+      let response = await fetch(`/groups/join/${groupId}`, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({'group-pw': groupPw.value}),
+      });
+      response = await response.json();
+    
+      if(response.success == true) {
+        joinLeaveButton.innerHTML = `
+        LEAVE GROUP
+      <span class="blob-btn__inner">
+        <span class="blob-btn__blobs">
+          <span class="blob-btn__blob"></span>
+          <span class="blob-btn__blob"></span>
+          <span class="blob-btn__blob"></span>
+          <span class="blob-btn__blob"></span>
+        </span>
+        `;
+        joinLeaveButton.removeEventListener('click', () => joinButtonEvent(joinLeaveButton, group.group_id, type));
+        joinLeaveButton.addEventListener('click', () => DelayModalEvent());
+        setTimeout(() => {
+          joinLeaveButton.addEventListener('click', () => leaveButtonEvent(joinLeaveButton, groupId, type));
+          joinLeaveButton.removeEventListener('click', () => DelayModalEvent());
+        }, 1000 * 60 * 10);
+        pwAskingModal.style = "display: none";
+      }
+    })
   }
 }
-
-const closeBtn = document.querySelector(".close-btn");
-const modal = document.querySelector('.subject-modal');
-
-const DelayModalEvent = () => {
-  modal.style = 'display: block';
-}
-
-closeBtn.addEventListener('click', () => {
-  modal.style =  "display: none";
-});
 
 
 
 
 //leave session event
 
-const leaveButtonEvent = async(joinLeaveButton, groupId) => {
+const leaveButtonEvent = async(joinLeaveButton, groupId, type) => {
   let response = await fetch(`/groups/leave/${groupId}`, {
     method: 'post',
     headers: {
@@ -100,7 +130,7 @@ const leaveButtonEvent = async(joinLeaveButton, groupId) => {
     joinLeaveButton.removeEventListener('click', () => leaveButtonEvent(joinLeaveButton, group.group_id));
     joinLeaveButton.addEventListener('click', () => DelayModalEvent());
     setTimeout(() => {
-      joinLeaveButton.addEventListener('click', () => joinButtonEvent(joinLeaveButton, groupId));
+      joinLeaveButton.addEventListener('click', () => joinButtonEvent(joinLeaveButton, groupId, type));
       joinLeaveButton.removeEventListener('click', () => DelayModalEvent());
     }, 1000 * 60 * 10)
   }
@@ -144,8 +174,11 @@ function copyToClipboard(text) {
     group.tags = JSON.parse(group.tags);
     const tags = group.tags.map(tag => `<li>${tag}</li>`).join('');
     let joinButtonText = (groupWithUser.includes(group.group_id)) ? "Leave Group" : "Join Group";
+    if(groupWithUser.includes(group.group_id)){
+      const div = document.createElement("div")
+    }
     group.members = group.members.split(',').map(item => item.trim());
-    
+    const likeBtnText = (group.likes?.includes(email)) ? `<svg version="1.0" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="64px" height="64px" viewBox="0 0 64 64" enable-background="new 0 0 64 64" xml:space="preserve" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path fill="#F76D57" d="M58.714,29.977c0,0-0.612,0.75-1.823,1.961S33.414,55.414,33.414,55.414C33.023,55.805,32.512,56,32,56 s-1.023-0.195-1.414-0.586c0,0-22.266-22.266-23.477-23.477s-1.823-1.961-1.823-1.961C3.245,27.545,2,24.424,2,21 C2,13.268,8.268,7,16,7c3.866,0,7.366,1.566,9.899,4.101l0.009-0.009l4.678,4.677c0.781,0.781,2.047,0.781,2.828,0l4.678-4.677 l0.009,0.009C40.634,8.566,44.134,7,48,7c7.732,0,14,6.268,14,14C62,24.424,60.755,27.545,58.714,29.977z"></path> <path fill="#F76D57" d="M58.714,29.977c0,0-0.612,0.75-1.823,1.961S33.414,55.414,33.414,55.414C33.023,55.805,32.512,56,32,56 s-1.023-0.195-1.414-0.586c0,0-22.266-22.266-23.477-23.477s-1.823-1.961-1.823-1.961C3.245,27.545,2,24.424,2,21 C2,13.268,8.268,7,16,7c3.866,0,7.366,1.566,9.899,4.101l0.009-0.009l4.678,4.677c0.781,0.781,2.047,0.781,2.828,0l4.678-4.677 l0.009,0.009C40.634,8.566,44.134,7,48,7c7.732,0,14,6.268,14,14C62,24.424,60.755,27.545,58.714,29.977z"></path> <g> <path fill="#394240" d="M48,5c-4.418,0-8.418,1.791-11.313,4.687l-3.979,3.961c-0.391,0.391-1.023,0.391-1.414,0 c0,0-3.971-3.97-3.979-3.961C24.418,6.791,20.418,5,16,5C7.163,5,0,12.163,0,21c0,3.338,1.024,6.436,2.773,9 c0,0,0.734,1.164,1.602,2.031s24.797,24.797,24.797,24.797C29.953,57.609,30.977,58,32,58s2.047-0.391,2.828-1.172 c0,0,23.93-23.93,24.797-24.797S61.227,30,61.227,30C62.976,27.436,64,24.338,64,21C64,12.163,56.837,5,48,5z M58.714,29.977 c0,0-0.612,0.75-1.823,1.961S33.414,55.414,33.414,55.414C33.023,55.805,32.512,56,32,56s-1.023-0.195-1.414-0.586 c0,0-22.266-22.266-23.477-23.477s-1.823-1.961-1.823-1.961C3.245,27.545,2,24.424,2,21C2,13.268,8.268,7,16,7 c3.866,0,7.366,1.566,9.899,4.101l0.009-0.009l4.678,4.677c0.781,0.781,2.047,0.781,2.828,0l4.678-4.677l0.009,0.009 C40.634,8.566,44.134,7,48,7c7.732,0,14,6.268,14,14C62,24.424,60.755,27.545,58.714,29.977z"></path> <path fill="#394240" d="M48,11c-0.553,0-1,0.447-1,1s0.447,1,1,1c4.418,0,8,3.582,8,8c0,0.553,0.447,1,1,1s1-0.447,1-1 C58,15.478,53.522,11,48,11z"></path> </g> </g> </g></svg>` : `<svg version="1.0" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 64 64" enable-background="new 0 0 64 64" xml:space="preserve" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path fill="#231F20" d="M48,6c-4.418,0-8.418,1.791-11.313,4.687l-3.979,3.961c-0.391,0.391-1.023,0.391-1.414,0 c0,0-3.971-3.97-3.979-3.961C24.418,7.791,20.418,6,16,6C7.163,6,0,13.163,0,22c0,3.338,1.024,6.436,2.773,9 c0,0,0.734,1.164,1.602,2.031s24.797,24.797,24.797,24.797C29.953,58.609,30.977,59,32,59s2.047-0.391,2.828-1.172 c0,0,23.93-23.93,24.797-24.797S61.227,31,61.227,31C62.976,28.436,64,25.338,64,22C64,13.163,56.837,6,48,6z M58.714,30.977 c0,0-0.612,0.75-1.823,1.961S33.414,56.414,33.414,56.414C33.023,56.805,32.512,57,32,57s-1.023-0.195-1.414-0.586 c0,0-22.266-22.266-23.477-23.477s-1.823-1.961-1.823-1.961C3.245,28.545,2,25.424,2,22C2,14.268,8.268,8,16,8 c3.866,0,7.366,1.566,9.899,4.101l0.009-0.009l4.678,4.677c0.781,0.781,2.047,0.781,2.828,0l4.678-4.677l0.009,0.009 C40.634,9.566,44.134,8,48,8c7.732,0,14,6.268,14,14C62,25.424,60.755,28.545,58.714,30.977z"></path> <path fill="#231F20" d="M48,12c-0.553,0-1,0.447-1,1s0.447,1,1,1c4.418,0,8,3.582,8,8c0,0.553,0.447,1,1,1s1-0.447,1-1 C58,16.478,53.522,12,48,12z"></path> </g> </g></svg>`;
     div.innerHTML = `
     <div class="group-inner">
     <div class="name">${lock}<p> ${group.name}</p></div>
@@ -163,16 +196,8 @@ function copyToClipboard(text) {
       </div>
     </div>
     <div class="bottom-btns">
-      <button class="blob-btn submit" id = "start">
-        Add Like
-        <span class="blob-btn__inner">
-          <span class="blob-btn__blobs">
-            <span class="blob-btn__blob"></span>
-            <span class="blob-btn__blob"></span>
-            <span class="blob-btn__blob"></span>
-            <span class="blob-btn__blob"></span>
-          </span>
-        </span>
+      <button class="blob-btn submit" id = "like${index}">
+        ${likeBtnText}
       </button>
       <button class="blob-btn submit" id = "share${index}">
         Share
@@ -199,17 +224,35 @@ function copyToClipboard(text) {
     </div>
   </div>
     `
-    groupsWrapper.appendChild(div);
+    if(groupWithUser.includes(group.group_id)){
+      mygroupsWrapper.appendChild(div);
+    } else {
+      groupsWrapper.appendChild(div);
+    }
     const joinLeaveButton = document.querySelector(`button#join${index}`);
     if(!groupWithUser.includes(group.group_id)){
-      joinLeaveButton.addEventListener('click', () => joinButtonEvent(joinLeaveButton, group.group_id));
+      joinLeaveButton.addEventListener('click', () => joinButtonEvent(joinLeaveButton, group.group_id, group.visibility));
     } else {
-      joinLeaveButton.addEventListener('click', () => leaveButtonEvent(joinLeaveButton, group.group_id));
+      joinLeaveButton.addEventListener('click', () => leaveButtonEvent(joinLeaveButton, group.group_id, group.visibility));
     }
 
     const shareButton = document.querySelector(`button#share${index}`);
     shareButton.addEventListener('click', () => {
       copyToClipboard(window.location.protocol + window.location.hostname + '/links/join/' + group.group_id);
+    })
+
+    const likeButton = document.querySelector(`button#like${index}`);
+    likeButton.addEventListener('click', async() => {
+      let response = await fetch(`/groups/like/${group.group_id}`, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+      })
+
+      response = await response.json();
+
+
     })
   });
   const endTime = performance.now();
