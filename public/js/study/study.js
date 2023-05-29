37293a1a97aacb76eef340c10d8b7bfb8337ef10
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2023-05-29 03:38:28 -0700

    timer finalized

--- public/js/study/study.js
+++ public/js/study/study.js
@@ -304,6 +304,7 @@ socket.on('studying', (userId, groups) => {
     const groupInfo = groupList.find(groupObj => groupObj.group_id == group);
     const memberInfo = groupInfo.members.find(member => member.userId === userId);
     memberInfo.timer.run = true;
+    console.log('timer', memberInfo.timer)
     memberInfo.timer.timer = setInterval(function () {countMember(memberInfo.timer);}, 10);
     //document.querySelector
     const memberSliders = document.querySelectorAll(`.swiper-slide#${group} ul li#${userId}`);
@@ -319,6 +320,10 @@ socket.on('studying', (userId, groups) => {
 socket.on('stopstudying', (userId, groups) => {
   console.log(userId, groups);
   groups.forEach((group) => {
+    const groupInfo = groupList.find(groupObj => groupObj.group_id == group);
+    const memberInfo = groupInfo.members.find(member => member.userId === userId);
+    memberInfo.timer.run = false;
+    clearInterval(memberInfo.timer.timer)
     const memberSliders = document.querySelectorAll(`.swiper-slide#${group} ul li#${userId}`);
     memberSliders.forEach((slider) => {
     slider.querySelector('.svg-area').innerHTML = `
@@ -338,6 +343,7 @@ socket.on('sendTime', (userId) => {
   });
   //socket.emit('timeResponse', )
 })
+console.log(document.querySelector('span'))
 
 
 const swiperWrapper = document.querySelector('.swiper-wrapper');
@@ -374,21 +380,10 @@ var groupList;
       `
       swiperWrapper.appendChild(swiperSlide);
       //group.members = JSON.parse(`[${group.members}]`) || [];
-      group.members.forEach(member => {
-        member.timer = {
-          hundredth: 0,
-          seconds: Math.floor((member.today / 1000) % 60),
-          minutes: Math.floor((member.today / (1000 * 60)) % 60),
-          hours: Math.floor((member.today / (1000 * 60 * 60))),
-          run: true,
-          timer: null,
-          secDisp: null,
-          minDisp: null,
-          hrDisp: null,
-          playBtn: null,
-        }
+      group.members.forEach((member, member_index) => {
         const membersWrapper = swiperSlide.querySelector('ul');
         const membersli = document.createElement('li');
+        membersli.setAttribute('id', member.userId);
         membersli.innerHTML = `
         <div class="member">
           <div class="member-name">${member.name}</div>
@@ -397,28 +392,48 @@ var groupList;
         </div>
           <div class="member-time">
             <div id="digits">
-              <span class="m-hr-${member.userId}"></span>
-              <span class="m-min-${member.userId}"></span>
-              <span class="m-sec-${member.userId}"></span>
+              <span class ="m-hr"></span>
+              <span class="m-min"></span>
+              <span class="m-sec"></span>
             </div>
           </div>
         </div>
         
-        `
-        membersli.setAttribute('id', member.userId);
+        `;
         membersWrapper.appendChild(membersli)
+        setTimeout(() => {
+          member.timer = {
+            hundredth: 0,
+            seconds: Math.floor((member.today / 1000) % 60),
+            minutes: Math.floor((member.today / (1000 * 60)) % 60),
+            hours: Math.floor((member.today / (1000 * 60 * 60))),
+            run: true,
+            timer: null,
+            secDisp: document.querySelectorAll(`#${member.userId} .m-sec`),
+            minDisp: document.querySelectorAll(`#${member.userId} .m-min`),
+            hrDisp: document.querySelectorAll(`#${member.userId} .m-hr`),
+            playBtn: null,
+          }
+          console.log(membersWrapper, membersli)
+          console.log(member.timer.secDisp, member.timer.minDisp, member.timer.hrDisp)
+          member.timer.secDisp.forEach(el => {
+            el.innerHTML = member.timer.seconds.toString().padStart(2, '0');
+          })
+          member.timer.minDisp.forEach(el => {
+            el.innerHTML = member.timer.minutes.toString().padStart(2, '0') + ":";
+          })
+          member.timer.hrDisp.forEach(el => {
+            el.innerHTML = member.timer.hours.toString().padStart(2, '0') + ":";
+          })
+          /* member.timer.secDisp.innerHTML = member.timer.seconds.toString().padStart(2, '0');
+          member.timer.minDisp.innerHTML = member.timer.minutes.toString().padStart(2, '0') + ":";
+          member.timer.hrDisp.innerHTML = member.timer.hours.toString().padStart(2, '0') + ":"; */
+          console.log(member.timer.secDisp, member.timer.minDisp, member.timer.hrDisp)
+          console.log(document.querySelector('span'))
+          console.log(member.timer.secDisp)
+        }, 0)
         
 
-        member.timer.secDisp = document.querySelector('.m-sec-' + member.userId);
-        member.timer.minDisp = document.querySelector('.m-min-' + member.userId);
-        member.timer.hrDisp = document.querySelector('.m-hr-' + member.userId);
-        member.timer.secDisp.innerHTML = member.timer.seconds.toString().padStart(2, '0');
-        member.timer.minDisp.innerHTML = member.timer.minutes.toString().padStart(2, '0') + ":";
-        member.timer.hrDisp.innerHTML = member.timer.hours.toString().padStart(2, '0') + ":";
-        /* member.timer.secDisp.innerHTML = member.timer.seconds.toString().padStart(2, '0');
-        member.timer.minDisp.innerHTML = member.timer.minutes.toString().padStart(2, '0') + ":";
-        member.timer.hrDisp.innerHTML = member.timer.hours.toString().padStart(2, '0') + ":"; */
-        console.log(member.timer.secDisp)
       })
       socket.emit('joinRoom', group.group_id, userId);
       console.log(group.group_id)
@@ -455,6 +470,7 @@ function toggleTimer(index) {
 
   } else {
     timer.timer = setInterval(function () { count(index); }, 10);
+    console.log(timer)
     timer.playBtn.innerHTML = `<span class="material-symbols-outlined"><svg fill="#000000" width="40px" height="40px" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M46.677 64.652c0-9.362 7.132-17.387 16.447-17.394 9.315-.007 24.677.007 34.55.007 9.875 0 17.138 7.594 17.138 16.998 0 9.403-.083 119.094-.083 127.82 0 8.726-7.58 16.895-16.554 16.837-8.975-.058-25.349.115-34.963.058-9.614-.058-16.646-7.74-16.646-17.254 0-9.515.11-117.71.11-127.072zm14.759.818s-.09 118.144-.09 123.691c0 5.547 3.124 5.315 6.481 5.832 3.358.518 21.454.47 24.402.47 2.947 0 7.085-1.658 7.167-6.14.08-4.483-.082-119.507-.082-123.249 0-3.742-4.299-4.264-7.085-4.66-2.787-.395-25.796 0-25.796 0l-4.997 4.056zm76.664-.793c.027-9.804 7.518-17.541 17.125-17.689 9.606-.147 25.283.148 35.004.148 9.72 0 17.397 8.52 17.397 17.77s-.178 117.809-.178 127c0 9.192-7.664 17.12-16.323 17.072-8.66-.05-26.354 0-34.991.048-8.638.05-17.98-8.582-18.007-17.783-.027-9.201-.055-116.763-.027-126.566zm16.917.554s-.089 118.145-.089 123.692c0 5.547 3.123 5.314 6.48 5.832 3.359.518 21.455.47 24.402.47 2.948 0 7.086-1.659 7.167-6.141.081-4.482-.08-119.506-.08-123.248 0-3.742-4.3-4.265-7.087-4.66-2.786-.396-25.796 0-25.796 0l-4.997 4.055z" fill-rule="evenodd"></path> </g></svg></span>`;
     timer.run = true;
     const activatedBtn = document.querySelector(`#drag-item-${index}`);
@@ -520,7 +536,8 @@ function count(index) {
 }
 
 function countMember(membertimer) {
-
+  //let membertimer = groupList
+  
   membertimer.hundredth += 1;
 
   if (membertimer.hundredth == 100) {
@@ -548,16 +565,19 @@ function disp(hun, sec, min, hr, index) {
   timer.hrDisp.innerHTML = hr.toLocaleString(undefined, { minimumIntegerDigits: 2 }) + ':';
 }
 
-function membersdisp(hun, sec, min, hr, membertimer) {
-  /* timer.secDisp.innerHTML = sec.toLocaleString(undefined, { minimumIntegerDigits: 2 });
-  timer.minDisp.innerHTML = min.toLocaleString(undefined, { minimumIntegerDigits: 2 }) + ':';
-  timer.hrDisp.innerHTML = hr.toLocaleString(undefined, { minimumIntegerDigits: 2 }) + ':'; */
-  membertimer.secDisp.innerHTML = sec.toLocaleString(undefined, { minimumIntegerDigits: 2 });
-  
+function membersdisp(hun, sec, min, hr, memberTimer) {
+  memberTimer.secDisp.forEach(el => {
+    el.innerHTML = sec.toLocaleString(undefined, { minimumIntegerDigits: 2 });
+  })
+  memberTimer.minDisp.forEach(el => {
+    el.innerHTML = min.toLocaleString(undefined, { minimumIntegerDigits: 2 }) + ':';
+  })
+  memberTimer.hrDisp.forEach(el => {
+    el.innerHTML = hr.toLocaleString(undefined, { minimumIntegerDigits: 2 }) + ':';
+  })
+  /* membertimer.secDisp.innerHTML = sec.toLocaleString(undefined, { minimumIntegerDigits: 2 });
   membertimer.minDisp.innerHTML = min.toLocaleString(undefined, { minimumIntegerDigits: 2 }) + ':';
-  
-  membertimer.hrDisp.innerHTML = hr.toLocaleString(undefined, { minimumIntegerDigits: 2 }) + ':';
-  
+  membertimer.hrDisp.innerHTML = hr.toLocaleString(undefined, { minimumIntegerDigits: 2 }) + ':'; */
 }
 
 const addSubjectBtn = document.querySelectorAll(".add-subject");
