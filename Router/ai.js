5f0f4afcf81c0e508b8e8d3a098bf4799262bcf8
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2023-06-10 03:11:22 -0700

    added add-plan-modal, ui completed. Err: document.execcommand weird, select to div for more style options

--- Router/ai.js
+++ Router/ai.js
@@ -3,16 +3,16 @@ const express = require('express');
 const Router = express.Router();
 const pool = require('../model/pool');
 
-Router.post('/activity', async (req, res) => {
+// Endpoint for generating activity recommendations
+Router.post('/recommendations', async(req, res) => {
+  userId = req.session.user_id;
   try {
-    // Extract user activity data from the request
-    const { userId, activities } = req.body;
-
-    // Get user activity data from the database
-    const userInfo = await getUserInfo(userId);
-
-    // Process user activity data and generate recommendations
-    const recommendations = generateRecommendations(userId, activities, userInfo);
+    // Extract user timeline data from the request
+    const connection = await (await pool).getConnection();
+    const userInfo = await connection.query(`SELECT subjects from users where user_id = ${userId}`)
+    console.log(userInfo)
+    // Generate activity recommendations based on the user's timeline
+    const recommendations = generateRecommendations(userId, userInfo.subjects);
 
     // Send recommendations as the response
     res.json(recommendations);
@@ -22,29 +22,21 @@ Router.post('/activity', async (req, res) => {
   }
 });
 
-async function getUserInfo(userId) {
-  const connection = await (await pool).getConnection();
-  let userInfo = await connection.query(`SELECT subjects FROM users WHERE user_id = '${userId}'`);
-  userInfo = userInfo[0];
-  userInfo.subjects = userInfo.subjects ? JSON.parse(userInfo.subjects) : [];
-  return userInfo;
-}
-
-function generateRecommendations(userId, activities, userInfo) {
-  // Determine the number of unique activities
-  const numActivities = calculateNumActivities(activities);
+function generateRecommendations(userId, timeline) {
+  // Extract activity names from the user's timeline
+  const activities = timeline.map((activity) => activity.name);
 
   // Build the collaborative filtering model
-  const model = buildModel(numActivities);
+  const model = buildModel();
 
   // Prepare training data
-  const trainingData = prepareTrainingData(userId, activities, numActivities);
+  const trainingData = prepareTrainingData(userId, timeline);
 
   // Train the model
   trainModel(model, trainingData);
 
   // Generate recommendations
-  const recommendations = generateUserRecommendations(model, userId);
+  const recommendations = generateUserRecommendations(model, userId, activities);
 
   // Return the recommendations
   return {
@@ -53,68 +45,49 @@ function generateRecommendations(userId, activities, userInfo) {
   };
 }
 
-function calculateNumActivities(activities) {
-  // Determine the number of unique activities
-  const uniqueActivities = new Set(activities.map((activity) => activity.name));
-  return uniqueActivities.size;
-}
-
-function buildModel(numActivities) {
+function buildModel() {
   // Define the model architecture
   const model = tf.sequential();
-  model.add(tf.layers.dense({ units: 32, activation: 'relu', inputShape: [numActivities] }));
-  model.add(tf.layers.dense({ units: 16, activation: 'relu' }));
-  model.add(tf.layers.dense({ units: numActivities, activation: 'softmax' }));
+  // Add layers and configurations as per your model requirements
 
   // Compile the model
-  model.compile({ loss: 'categoricalCrossentropy', optimizer: 'adam' });
+  model.compile({ loss: 'meanSquaredError', optimizer: 'adam' });
 
   return model;
 }
 
-async function prepareTrainingData(userId, activities, numActivities) {
-  // Prepare the training data based on user activities
-  // Each activity is converted into a one-hot encoded vector
+function prepareTrainingData(userId, timeline) {
+  // Prepare the training data based on the user's timeline
 
-  const trainingData = [];
-  if (Array.isArray(activities)) {
-    for (const activity of activities) {
-      const activityVector = createActivityVector(activity, numActivities);
-      trainingData.push({ userId, activity: activityVector });
-    }
-  }
+  // Here, you can use the timeline data to create training examples
+  // For example, you can encode the timeline into feature vectors and create corresponding target vectors
+  // The feature vectors can represent the user's activity history, and the target vectors can represent the next activity in the timeline
+
+  // Implement your data preparation logic here
 
   return trainingData;
 }
 
-function createActivityVector(activity, numActivities) {
-  // Create a one-hot encoded vector for the activity
-
-  const activityVector = Array(numActivities).fill(0);
-  const activityIndex = activity.name.charCodeAt(0) % numActivities;
-  activityVector[activityIndex] = 1;
-
-  return activityVector;
-}
-
 function trainModel(model, trainingData) {
   // Train the model using the training data
+
   // Convert the training data into TensorFlow tensors
 
-  const xs = tf.tensor2d(trainingData.map((data) => data.userId), [trainingData.length, 1]);
-  const ys = tf.tensor2d(trainingData.map((data) => data.activity), [trainingData.length, trainingData[0].activity.length]);
+  const xs = tf.tensor2d(/* Input feature vectors */);
+  const ys = tf.tensor2d(/* Target vectors */);
 
   // Train the model
   model.fit(xs, ys, { epochs: 10 });
 }
 
-function generateUserRecommendations(model, userId) {
+function generateUserRecommendations(model, userId, activities) {
   // Generate recommendations for the user
-  // Use the trained model to predict user preferences
 
-  const userVector = tf.tensor2d([[userId]]);
-  const predictions = model.predict(userVector);
-  const recommendations = predictions.arraySync()[0];
+  // Use the trained model to predict the next activity in the timeline
+  // You can input the user's activity history (encoded as feature vectors) into the model and obtain predictions
+  // The predictions can represent the likelihood or scores of different activities
+
+  // Implement your recommendation generation logic here
 
   return recommendations;
 }
