5ff1ed098458108ffe324a222b0e58a0f074e196
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2023-08-21 21:13:24 +0000

    Groups ver 0

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Main from './components/Container/Main/Main';
 import Stats from './components/Container/Stats/Stats';
 import Ranking from './components/Container/Ranking/Ranking';
+import Groups from './components/Container/Groups/Groups';
 import './App.css';
 import Sidebar from './components/UI/Sidebar/Sidebar';
 import Header from './components/UI/Header/Header';
@@ -25,44 +26,56 @@ function App() {
   };
   return (
     <Router>
-        <Routes>
-          <Route path="/dashboard" element={
-            <div>
-              <Sidebar isSidebarOpen={isSidebarOpen}
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-                isSidebarHovered={isHovered}
-              />
-              <Header onToggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} isSidebarHovered={isHovered} />
-              <Main setIsSidebarOpen={setIsSidebarOpen} isSidebarOpen={isSidebarOpen} isSidebarHovered={isHovered}/>
-              <Footer />
-            </div>
-          } />
-          <Route path="/dashboard/stats" element={
-            <div>
-              <Sidebar isSidebarOpen={isSidebarOpen}
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-                isSidebarHovered={isHovered}
-              />
-              <Header onToggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} isSidebarHovered={isHovered} />
-              <Stats setIsSidebarOpen={setIsSidebarOpen} isSidebarOpen={isSidebarOpen} isSidebarHovered={isHovered}/>
-              <Footer />
-            </div>
-          } />
-          <Route path="/dashboard/ranking" element={
-            <div>
-              <Sidebar isSidebarOpen={isSidebarOpen}
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-                isSidebarHovered={isHovered}
-              />
-              <Header onToggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} isSidebarHovered={isHovered} />
-              <Ranking setIsSidebarOpen={setIsSidebarOpen} isSidebarOpen={isSidebarOpen} isSidebarHovered={isHovered}/>
-              <Footer />
-            </div>
-          } />
-        </Routes>
+      <Routes>
+        <Route path="/dashboard" element={
+          <div>
+            <Sidebar isSidebarOpen={isSidebarOpen}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+              isSidebarHovered={isHovered}
+            />
+            <Header onToggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} isSidebarHovered={isHovered} />
+            <Main setIsSidebarOpen={setIsSidebarOpen} isSidebarOpen={isSidebarOpen} isSidebarHovered={isHovered} />
+            <Footer />
+          </div>
+        } />
+        <Route path="/dashboard/stats" element={
+          <div>
+            <Sidebar isSidebarOpen={isSidebarOpen}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+              isSidebarHovered={isHovered}
+            />
+            <Header onToggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} isSidebarHovered={isHovered} />
+            <Stats setIsSidebarOpen={setIsSidebarOpen} isSidebarOpen={isSidebarOpen} isSidebarHovered={isHovered} />
+            <Footer />
+          </div>
+        } />
+        <Route path="/dashboard/ranking" element={
+          <div>
+            <Sidebar isSidebarOpen={isSidebarOpen}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+              isSidebarHovered={isHovered}
+            />
+            <Header onToggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} isSidebarHovered={isHovered} />
+            <Ranking setIsSidebarOpen={setIsSidebarOpen} isSidebarOpen={isSidebarOpen} isSidebarHovered={isHovered} />
+            <Footer />
+          </div>
+        } />
+        <Route path="/dashboard/groups" element={
+          <div>
+            <Sidebar isSidebarOpen={isSidebarOpen}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+              isSidebarHovered={isHovered}
+            />
+            <Header onToggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} isSidebarHovered={isHovered} />
+            <Groups setIsSidebarOpen={setIsSidebarOpen} isSidebarOpen={isSidebarOpen} isSidebarHovered={isHovered} />
+            <Footer />
+          </div>
+        } />
+      </Routes>
     </Router>
   );
 }
