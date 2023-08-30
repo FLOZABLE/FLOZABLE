91a923e1aebf9ca7b3a2f7bdf026e49d24b74398
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2023-08-29 23:21:02 -0700

    group join ver 1

---
+++ app/src/components/UI/LikeBtn/LikeBtn.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import styles from "./LikeBtn.module.css";
+
+const LikeBtn = () => {
+  const [liked, setLiked] = useState(false);
+
+  const handleLike = () => {
+
+    setLiked(!liked);
+    /* setTimeout(() => {
+      setLiked(false);
+    }, 1000); */
+  };
+
+  return (
+    <div className={styles.middleWrapper}>
+      <div className={styles.likeWrapper}>
+        <a
+          className={`${styles.likeButton} ${liked ? styles.liked : ''}`}
+          onClick={handleLike}
+        >
+          <span className={styles.likeIcon}>
+            <div className={styles.heartAnimation1}></div>
+            <div className={styles.heartAnimation2}></div>
+          </span>
+          Favorite
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default LikeBtn;
