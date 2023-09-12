7e23bfd1608ca73848a1fecaeda216bb49adacab
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2023-09-12 01:51:31 -0700

    30% color palette

--- app/src/components/UI/AddSubjectModal/AddSubjectModal.js
+++ app/src/components/UI/AddSubjectModal/AddSubjectModal.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./AddSubjectModal.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBook, faXmark } from "@fortawesome/free-solid-svg-icons";
+import CustomInput from "../CustomInput/CustomInput";
+import ColorPalette from "../ColorPalette/ColorPalette";
 
 function AddSubjectModal(props) {
+  const {isAddSubjectModal, setIsAddSubjectModal} = props;
+
+  const [name, setName] = useState('');
+
+  const handleNameInput = (e) => {
+    setName(e.target.value);
+  }
   return (
-    <div className={styles.AddSubjectModal}>
+    <div className={`${styles.AddSubjectModal} ${isAddSubjectModal ? styles.open : ''}`}>
+      <div className={styles.header}>
+        <i>
+        <FontAwesomeIcon icon={faXmark} />
+        </i>
+      </div>
+      <div className={styles.content}>
+        <CustomInput input={name} handleInput={handleNameInput} icon={faBook} placeHolder={"Subject Name"} type={"text"} />
+        <ColorPalette />
+      </div>
 
     </div>
   )
---
+++ app/src/utils/ColorPaletteOptions.js
@@ -0,0 +1,28 @@
+const ColorPaletteOptions = [
+  {
+    name: 'Mustard',
+    colors: ['#FFD151', '#FFEAAF', '#FFE190', '#FFD970']
+  },
+  {
+    name: 'Mustard',
+    colors: ['#FFD151', '#FFEAAF', '#FFE190', '#FFD970']
+  },
+  {
+    name: 'Mustard',
+    colors: ['#FFD151', '#FFEAAF', '#FFE190', '#FFD970']
+  },
+  {
+    name: 'Mustard',
+    colors: ['#FFD151', '#FFEAAF', '#FFE190', '#FFD970']
+  },
+  {
+    name: 'Mustard',
+    colors: ['#FFD151', '#FFEAAF', '#FFE190', '#FFD970']
+  },
+  {
+    name: 'Mustard',
+    colors: ['#FFD151', '#FFEAAF', '#FFE190', '#FFD970']
+  },
+];
+
+export {ColorPaletteOptions};
