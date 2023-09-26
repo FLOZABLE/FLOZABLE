f5c478b783a5cd50319ae8cc62f1f629c8c65d47
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2023-09-26 21:18:53 +0000

    groups mygroupsviewr err fixed, other subjects timer err found

--- app/src/components/UI/MemberTimer/MemberTimer.js
+++ app/src/components/UI/MemberTimer/MemberTimer.js
@@ -29,9 +29,11 @@ function MemberTimer(props) {
 
   useEffect(() => {
     if (me) {
-      setSec(myTimerTotal % 60);
-      setMin(Math.floor(myTimerTotal / 60) % 60);
-      setHr(Math.floor(myTimerTotal / (60 * 60)));
+      if (!myTimerTotal) {
+        setSec(myTimerTotal % 60);
+        setMin(Math.floor(myTimerTotal / 60) % 60);
+        setHr(Math.floor(myTimerTotal / (60 * 60)));
+      };
     }
   }, [myTimerTotal]);
 
