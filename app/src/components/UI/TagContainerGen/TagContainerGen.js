748ca8cd1d5e24844e10f23872467da3d84fc19e
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2023-09-30 05:37:11 -0700

    message modal ver 1

--- app/src/components/UI/TagContainerGen/TagContainerGen.js
+++ app/src/components/UI/TagContainerGen/TagContainerGen.js
@@ -1,101 +1,91 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from "./TagContainerGen.module.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTags, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faTags, faTimes } from '@fortawesome/free-solid-svg-icons';
 
-class TagContainerGen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      maxTags: props.maxTags,
-      createdTags: [],
-      inputValue: '',
-      tagCount: props.maxTags
-    };
-    this.addTag = this.addTag.bind(this);
-    this.remove = this.remove.bind(this);
-    this.createTag = this.createTag.bind(this);
+const TagContainerGen = (props) => {
+
+  const { handleCreatedTagsChange } = props;
+
+  const [maxTags] = useState(props.maxTags);
+  const [createdTags, setCreatedTags] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [tagCount, setTagCount] = useState(maxTags);
+
+  useEffect(() => {
+    countTags();
+  }, [createdTags]);
+
+  const countTags = () => {
+    setTagCount(maxTags - createdTags.length);
+  };
+
+  const remove = (tag) => {
+    const updatedTags = createdTags.filter(t => t !== tag);
+    setCreatedTags(updatedTags);
+    handleCreatedTagsChange(updatedTags); // Call the callback to update the state in the parent component
+    countTags();
+  };
+
+  const removeAll = () => {
+    setCreatedTags([]);
+    handleCreatedTagsChange([]);
   }
 
-  componentDidMount() {
-    this.countTags();
-  }
-
-  countTags() {
-    this.setState({ tagCount: this.state.maxTags - this.state.createdTags.length });
-  }
-
-  remove(tag) {
-    const updatedTags = this.state.createdTags.filter(t => t !== tag);
-    this.setState({ createdTags: updatedTags }, () => {
-      this.createTag();
-      this.props.handleCreatedTagsChange(updatedTags); // Call the callback to update the state in the parent component
-      this.countTags();
-    });
-  }
-
-  createTag() {
-    return this.state.createdTags.map((tag, index) => (
+  const createTag = () => {
+    return createdTags.map((tag, index) => (
       <li key={index}>
         <p className={styles.tags}>{tag}</p>
-        <FontAwesomeIcon icon={faXmark} className={styles.closeIcon} onClick={() => this.remove(tag)}/>
+        <FontAwesomeIcon icon={faTimes} className={styles.closeIcon} onClick={() => remove(tag)} />
       </li>
     ));
-  }
+  };
 
-  addTag(e) {
+  const addTag = (e) => {
     if (e.key === 'Enter') {
       const tag = e.target.value.trim();
-      if (tag.length > 1 && !this.state.createdTags.includes(tag)) {
-        if (this.state.createdTags.length < 10) {
+      if (tag.length > 1 && !createdTags.includes(tag)) {
+        if (createdTags.length < 10) {
           const newTags = tag.split(',').map(t => t.trim());
-          this.setState(
-            prevState => ({ createdTags: [...prevState.createdTags, ...newTags] }),
-            () => {
-              this.createTag();
-              this.setState({ inputValue: '' });
-              this.countTags();
-            }
-          );
+          setCreatedTags(prevState => [...prevState, ...newTags]);
+          setInputValue('');
+          handleCreatedTagsChange([...createdTags, ...newTags]);
+          countTags();
         }
       }
     }
-  }
+  };
 
-  render() {
-    return (
-      <div className={styles.TagContainerGen}>
-        <div className={styles.title}>
-          <i className={`fa-solid fa-tags ${styles.icon}`}></i>
-          <FontAwesomeIcon icon={faTags} className={styles.faTags}/>
-          <h2>Tags</h2>
-        </div>
-        <div className={styles.content}>
-          <p>Press enter after each tag</p>
-          <ul className={styles.tags}>
-          {this.createTag()}
-            <input
-              className={styles.tags}
-              type="text"
-              spellCheck="false"
-              onKeyUp={this.addTag}
-              value={this.state.inputValue}
-              onChange={e => this.setState({ inputValue: e.target.value })}
-            />
-          </ul>
-        </div>
-        <div className={styles.details}>
-          <p>
-            <span>{this.state.tagCount}</span> tags are remaining
-          </p>
-          <button className={styles.removeAllBtn} onClick={() => this.setState({ createdTags: [] }, this.countTags)}>
-            Remove All
-          </button>
-        </div>
-        {/* <ul className={styles.tagsList}>{this.createTag()}</ul> */}
+  return (
+    <div className={styles.TagContainerGen}>
+      <div className={styles.title}>
+        <FontAwesomeIcon icon={faTags} className={styles.faTags} />
+        <h2>Tags</h2>
       </div>
-    );
-  }
-}
+      <div className={styles.content}>
+        <p>Press enter after each tag</p>
+        <ul className={styles.tags}>
+          {createTag()}
+          <input
+            className={styles.tagsInput}
+            type="text"
+            spellCheck="false"
+            onKeyUp={addTag}
+            value={inputValue}
+            onChange={e => setInputValue(e.target.value)}
+          />
+        </ul>
+      </div>
+      <div className={styles.details}>
+        <p>
+          <span>{tagCount}</span> tags are remaining
+        </p>
+        <button className={styles.removeAllBtn} onClick={() => removeAll()}>
+          Remove All
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default TagContainerGen;
