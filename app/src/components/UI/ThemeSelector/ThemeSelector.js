53017e9a0b0e7c01bb88b5226913504823aede0e
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2023-11-01 04:34:56 -0700

    draggable study tools

---
+++ app/src/components/UI/ThemeSelector/ThemeSelector.js
@@ -0,0 +1,23 @@
+import styles from "./ThemeSelector.module.css";
+import { AllThemes } from "../../../utils/Themes";
+import CustomInput from "../CustomInput/CustomInput";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+
+function ThemeSelector({link, handleLinkInput, submit, setVideoId}) {
+  return (
+    <div className={styles.ThemeSelector}>
+      <div className={styles.themeContainer}>
+      {AllThemes.map((Theme, i) => {
+        return (
+          <div className={styles.video} key={i} onClick={() => { setVideoId(Theme.id) }}>
+            <img src={Theme.img} alt={Theme.id} />
+          </div>
+        );
+      })}
+      </div>
+      <CustomInput input={link} handleInput={handleLinkInput} handleEnter={submit} icon={faLink} placeHolder={"Paste a Youtube Link"} type={"text"} />
+    </div>
+  );
+};
+
+export default ThemeSelector;
