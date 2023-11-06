a99f14bb52df3a26dd8b9d55094bed7507156b80
HackingGo306 <121586285+HackingGo306@users.noreply.github.com>
2023-11-06 20:36:26 +0000

    study sidebar v0

--- app/src/components/UI/StudyModalContainer/StudyModalContainer.js
+++ app/src/components/UI/StudyModalContainer/StudyModalContainer.js
@@ -1,10 +1,10 @@
 import { useRef } from "react";
 import styles from "./StudyModalContainer.module.css";
 import Draggable from 'react-draggable';
-function StudyModalContainer ({element, setIsDisp, isDisp}) {
+function StudyModalContainer ({element, setIsDisp, isDisp, startPos}) {
   const ref = useRef();
   return (
-    <Draggable nodeRef={ref}>
+    <Draggable positionOffset={{x: startPos.x, y: startPos.y}} nodeRef={ref}>
     <div ref={ref} className={`${styles.StudyModalContainer} ${isDisp ? styles.visible: ''}`}>
       <div className={styles.inner}>
         {element}
