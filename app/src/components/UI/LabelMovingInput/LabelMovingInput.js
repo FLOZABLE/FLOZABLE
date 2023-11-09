0956e6c70a9a204d87c8c8dc815fe36e42aea68e
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2023-11-08 22:59:06 -0800

    account page ver4

--- app/src/components/UI/LabelMovingInput/LabelMovingInput.js
+++ app/src/components/UI/LabelMovingInput/LabelMovingInput.js
@@ -3,8 +3,10 @@ import styles from "./LabelMovingInput.module.css";
 function LabelMovingInput({ title, type, value, setValue }) {
   return (
     <div className={styles.LabelMovingInput}>
+      <form>
       <input type={type} defaultValue={value} onChange={(e) => {setValue(e.target.value)}}/>
       <label className={styles.label}>{title}</label>
+      </form>
     </div>
   );
 };
