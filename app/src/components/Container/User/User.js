b4cccb6eef01b77d630a9ef6236ae6df35106486
dIwnsgml <junjason1126@gmail.com>
2023-11-13 14:18:29 -0800

    account profile fetching ver 0

--- app/src/components/Container/User/User.js
+++ app/src/components/Container/User/User.js
@@ -5,6 +5,19 @@ import StuckModal from '../../UI/StuckModal/StuckModal';
 const serverOrigin = process.env.REACT_APP_ORIGIN;
 
 function User({ isSidebarOpen, isSidebarHovered }) {
+
+  useEffect(() => {
+    const pathName = window.location.pathname.split('/');
+    const selectedUserId = pathName[pathName.length - 1];
+    fetch(`${serverOrigin}/api/account/porfile/${selectedUserId}`, { method: 'post' })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success) {
+        setPlans(data.plans.map(plan => { plan.saved = true; plan.start = new Date(plan.start * 1000 * 60); plan.end = new Date(plan.end * 1000 * 60); return plan }));
+      };
+    })
+    .catch((error) => console.error(error));
+  }, []);
   return (
     <div className={styles.RankingContainer}>
       <StuckModal />
