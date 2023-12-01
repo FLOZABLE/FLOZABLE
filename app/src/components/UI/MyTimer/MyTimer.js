957051f89379398ab1970d5376b7e6bc6ab9acce
dIwnsgml <junjason1126@gmail.com>
2023-11-30 18:40:17 -0800

    member timer err

--- app/src/components/UI/MyTimer/MyTimer.js
+++ app/src/components/UI/MyTimer/MyTimer.js
@@ -1,22 +1,17 @@
 import React, { useEffect, useState, useRef } from "react";
 import styles from "./MyTimer.module.css";
-import worker from "./TimeWorker";
+import worker from "../../../utils/subjectTimerWorker";
 
-function MyTimer(props) {
-  const { run, total } = props;
+function MyTimer({ run, total }) {
   const [sec, setSec] = useState(0);
   const [min, setMin] = useState(0);
   const [hr, setHr] = useState(0);
 
   worker.addEventListener("message", (e) => {
-    if (run && e.data.command === "update-timer") {
+    if (run && e.data.command === "updateSubjectTimer") {
       setSec(sec + 1);
     }
   });
-
-  useEffect(() => {
-
-  }, []);
 
   useEffect(() => {
     console.log('gdt', total)
