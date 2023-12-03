36a0667607d06904a517cc5fc7d4165c0a28a9ff
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2023-12-02 19:50:29 -0800

    env/package files fixed

--- app/src/socket.js
+++ app/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io(process.env.REACT_APP_SERVER_ORIGIN, { autoConnect: false });
+const socket = io(process.env.REACT_APP_ORIGIN, { autoConnect: false });
 
 export { socket };
