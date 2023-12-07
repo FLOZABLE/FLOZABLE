597f457e68c3a4acdaf902d12b9eb772ffb94bfd
HackingGo306 <121586285+HackingGo306@users.noreply.github.com>
2023-12-07 02:58:12 +0000

    theme selector category

--- app/src/components/UI/ThemeSelector/ThemeSelector.js
+++ app/src/components/UI/ThemeSelector/ThemeSelector.js
@@ -2,33 +2,82 @@ import styles from "./ThemeSelector.module.css";
 import { AllThemes } from "../../../utils/Themes";
 import CustomInput from "../CustomInput/CustomInput";
 import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect } from "react";
 
 const serverOrigin = process.env.REACT_APP_ORIGIN;
 
 function ThemeSelector({ link, handleLinkInput, submit, setVideoId }) {
+
+  const [themeCategory, setThemeCategory] = useState("");
+  const [themeChoices, setThemeChoices] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectionEl, setSelectionEl] = useState(<p></p>)
+
+  useEffect(() => {
+    console.log(themeCategory);
+    if (themeCategory === "") {
+      setSelectionEl(
+        <div className={`${styles.themeContainer} customScroll`}>
+          {AllThemes.map((Theme, i) => {
+            return (
+              <div
+                className={styles.video}
+                key={i}
+                onClick={() => {
+                  setThemeCategory(Theme.category);
+                  setThemeChoices(Theme.choices);
+                }}
+                style={{
+                  backgroundImage: `url("${serverOrigin}/img/Themes/${Theme.img}")`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center center",
+                  backgroundRepeat: "no-repeat",
+                }}
+              >
+                <div className={styles.categoryLabel}>
+                  <p>{Theme.category}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )
+    }
+    else {
+      console.log(themeChoices);
+      setSelectionEl(
+        <div className={`${styles.themeContainer} customScroll`}>
+          {themeChoices.map((Theme, i) => {
+            return (
+              <div
+                className={styles.video}
+                key={i}
+                onClick={() => {
+                  setVideoId(Theme.id);
+                }}
+                style={{
+                  backgroundImage: `url("${serverOrigin}/img/Themes/${Theme.img}")`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center center",
+                  backgroundRepeat: "no-repeat",
+                }}
+              >
+                <div className={styles.nameLabel}>
+                  <p>{Theme.name}</p>
+                </div>
+              </div>
+            );
+          })}
+          <button onClick={() => {setThemeCategory("")}}>&lt;Back</button>
+          <button>Load More</button>
+        </div>
+      )
+    }
+  }, [themeChoices, themeCategory]);
+
   return (
     <div className={styles.ThemeSelector}>
-      <div className={`${styles.themeContainer} customScroll`}>
-        {AllThemes.map((Theme, i) => {
-          return (
-            <div
-              className={styles.video}
-              key={i}
-              onClick={() => {
-                setVideoId(Theme.id);
-              }}
-              style={{
-                backgroundImage: `url("${serverOrigin}/img/Themes/${Theme.img}")`,
-                backgroundSize: "cover",
-                backgroundPosition: "center center",
-                backgroundRepeat: "no-repeat",
-              }}
-            >
-              {/*  <img src={Theme.img} alt={Theme.id} /> */}
-            </div>
-          );
-        })}
-      </div>
+      {selectionEl}
       <CustomInput
         input={link}
         handleInput={handleLinkInput}
--- app/src/utils/Themes.js
+++ app/src/utils/Themes.js
@@ -1,10 +1,52 @@
 const AllThemes = [
-  { id: "MYPVQccHhAQ", img: "cafe.png" },
-  { id: "YQc4WT0yDH4", img: "library.png" },
-  { id: "0L38Z9hIi5s", img: "cafe2.png" },
-  { id: "HGl75kurxok", img: "ghibli.png" },
-  { id: "lTRiuFIWV54", img: "ani1.png" },
-  { id: "4vIQON2fDWM", img: "library2.png" }
+  { category: "Most Used", img: "cafe.png", choices: [
+    { id: "MYPVQccHhAQ", img: "cafe.png", name: "Coffee Shop" },
+    { id: "YQc4WT0yDH4", img: "library.png", name: "Bookstore" },
+    { id: "0L38Z9hIi5s", img: "cafe2.png", name: "Cafe" },
+    { id: "HGl75kurxok", img: "ghibli.png", name: "Anime" },
+    { id: "lTRiuFIWV54", img: "ani1.png", name: "Lofi" },
+    { id: "4vIQON2fDWM", img: "library2.png", name: "Library" },
+  ]},
+  { category: "1", img: "cafe.png", choices: [
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+  ]},
+  { category: "2", img: "cafe.png", choices: [
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+  ]},
+  { category: "3", img: "cafe.png", choices: [
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+  ]},
+  { category: "4", img: "cafe.png", choices: [
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+  ]},
+  { category: "5", img: "cafe.png", choices: [
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+    {id: "ddL3y1saEJs", img: "cafe.png"},
+  ]},
 ];
 
 export { AllThemes };
