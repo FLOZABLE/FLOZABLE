cccb43d8aa3708fb778c48b446aaff2e2233da38
HackingGo306 <121586285+HackingGo306@users.noreply.github.com>
2023-12-11 22:57:41 +0000

    challengerooms

---
+++ app/src/components/UI/ChallengeRoom/ChallengeRoom.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./ChallengeRoom.module.css";
+import { useEffect, useState } from "react";
+
+const serverOrigin = process.env.REACT_APP_ORIGIN;
+
+function ChallengeRoom({ challengeInfo, setResponse, setChallenges, challenges }) {
+
+    function joinChallenge(challengeId) {
+        setChallenges(challenges.filter((c) => c.id !== challengeId));
+        fetch(`${serverOrigin}/api/challenges/join-challenge`, {
+            method: "post",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ joinId: challengeId }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                setResponse(data);
+            })
+    }
+
+    return (
+        <div className={styles.ChallengeRoom}>
+            <h2>{challengeInfo.name}</h2>
+            <h3>{challengeInfo.description}</h3>
+            <p>By: {challengeInfo.userInfo.name}</p>
+            <p>Starts: {challengeInfo.start_date}</p>
+            <button onClick = {() => {joinChallenge(challengeInfo.id)}}>Join!</button>
+        </div>
+    );
+}
+
+export default ChallengeRoom;
