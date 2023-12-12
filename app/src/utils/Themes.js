631aeb1f26e3195523acae85fb5d8437536132ab
shan jiang <22camerjian@gmail.com>
2023-12-11 21:42:41 -0800

    themes

--- app/src/utils/Themes.js
+++ app/src/utils/Themes.js
@@ -1,16 +1,19 @@
 const AllThemes = [
-  { id: "MYPVQccHhAQ", img: "cafe.png", name: "Coffee Shop", category: ["Default"] },
-  { id: "YQc4WT0yDH4", img: "library.png", name: "Bookstore", category: ["Default"] },
-  { id: "0L38Z9hIi5s", img: "cafe2.png", name: "Cafe", category: ["Default"] },
-  { id: "HGl75kurxok", img: "ghibli.png", name: "Anime", category: ["Default"] },
-  { id: "lTRiuFIWV54", img: "ani1.png", name: "Lofi", category: ["Default"] },
-  { id: "4vIQON2fDWM", img: "library2.png", name: "Library", category: ["Default"] },
+  { id: "MYPVQccHhAQ", img: "cafe.png", name: "Cafe 1", category: ["Default", "Cafe"] },
+  { id: "0L38Z9hIi5s", img: "cafe2.png", name: "Cafe 2", category: ["Default", "Cafe"] },
+  { id: "fX7k3W-2BMM", img: "ghibli.png", name: "Ghibli", category: ["Default", "Anime"] },
+  { id: "HGl75kurxok", img: "ghibli.png", name: "Anime", category: ["Default", "Anime"] },
+  { id: "lTRiuFIWV54", img: "ani1.png", name: "Lofi 1", category: ["Default", "Lofi"] },
+  { id: "jfKfPfyJRdk", img: "ani1.png", name: "Lofi Stream", category: ["Default", "Lofi"]},
+  { id: "4vIQON2fDWM", img: "library2.png", name: "Library", category: ["Default", "Library"] },
+  { id: "YQc4WT0yDH4", img: "library.png", name: "Bookstore", category: ["Default", "Library"] },
 ];
 
 const AllCategories = [
   "Cafe:0",
   "Rain:1",
   "Anime:2",
+  "Lofi:3",
 ]
 AllCategories.sort((a, b) => {return a.localeCompare(b)}); //sort
 
