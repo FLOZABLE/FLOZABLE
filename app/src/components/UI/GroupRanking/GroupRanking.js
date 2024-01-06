82dd05c6a9ba1a1759fcd93e4394bcd3d6155f06
shan jiang <22camerjian@gmail.com>
2024-01-05 20:42:37 -0800

    group ranking raw html

--- app/src/components/UI/GroupRanking/GroupRanking.js
+++ app/src/components/UI/GroupRanking/GroupRanking.js
@@ -1,25 +1,60 @@
 import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./GroupRanking.module.css";
+import { durationFormatter } from "../../../utils/Tool.js";
+import { isRequestOptions } from "openai/core";
+import BlobBtn from "../BlobBtn/BlobBtn";
 
 const serverOrigin = process.env.REACT_APP_ORIGIN;
 
-function GroupRanking({ groupMembers }) {
-    const [searched, setSearched] = useState(false);
+function GroupRanking({ groupMembers, isGroupRankingModal, setIsGroupRankingModal }) {
+    const [groupRanking, setGroupRanking] = useState([]);
     useEffect(() => {
-        groupMembers.map((member) => {
-            fetch(`${serverOrigin}/account/profile/${selectedUserId}`, { method: 'get' })
-                .then((response) => response.json())
-                .then((data) => {
+        if (!!!groupMembers) return;
+        const groupMemberIds = [];
+        const tempGroupRanking = [];
 
+        groupMembers.map((member) => {
+            groupMemberIds.push(member.user_id);
+        })
+
+        fetch(`${serverOrigin}/ranking/today`, { method: 'get' })
+            .then((response) => response.json())
+            .then((data) => {
+                data.rankings.map((ranking) => {
+                    if (groupMemberIds.includes(ranking.user.user_id)) {
+                        tempGroupRanking.push(ranking);
+                    }
                 })
-                .catch((error) => console.error(error));
-        });
-    }, [])
+                setGroupRanking(tempGroupRanking);
+            })
+            .catch((error) => console.error(error));
+
+    }, [groupMembers]);
 
     return (
-        <div>
-            <p></p>
+        <div className={`${styles.GroupRanking}`}>
+            <div className={isGroupRankingModal ? styles.GroupRankingContainer : styles.hidden}>
+                <h1>Group Ranking</h1>
+                <div>
+                    {
+                        groupRanking.map((ranking, i) => {
+                            return (
+                                <div key={i}>
+                                    {i + 1}&#41; {ranking.user.name}: {durationFormatter(ranking.total)}
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <BlobBtn
+                  name={"Close"}
+                  setClicked={() => {setIsGroupRankingModal(!isGroupRankingModal)}}
+                  color1={"#fff"}
+                  color2={"var(--pink)"}
+                  delay={-1}
+                />
+            </div>
         </div>
     );
 }
