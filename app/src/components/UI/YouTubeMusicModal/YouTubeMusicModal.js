664a3380be36d0992194bc371693a51c05970b11
shan jiang <22camerjian@gmail.com>
2024-01-15 16:08:54 -0800

    background audio control

---
+++ app/src/components/UI/YouTubeMusicModal/YouTubeMusicModal.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import styles from "./YouTubeMusicModal.module.css";
+import YouTubeAudioPlayer from "../YoutubeAudioPlayer/YoutTubeAudioPlayer";
+import { YouTubeMusic } from "../../../utils/Music";
+import VolumeControl from "../VolumeControl/VolumeControl";
+//<YouTubeAudioPlayer height={"50%"} weight={"50%"} videoId={"nMfPqeZjc2c"} volume={tempVolume}/>
+function YouTubeMusicModal({ originalVideoVolume, setOriginalVideoVolume }) {
+
+  const [audioVolumes, setAudioVolumes] = useState([]);
+  const [audioChoices, setAudioChoices] = useState([])
+
+  useEffect(() => {
+    const tempAudioChoices = [...YouTubeMusic];
+    setAudioChoices(tempAudioChoices);
+  }, []);
+
+  useEffect(() => {
+    setAudioVolumes(new Array(audioChoices.length).fill[0]);
+  }, [audioChoices])
+
+  console.log(YouTubeMusic);
+
+  return (
+    <div className={`${styles.YouTubeMusicModal}`}>
+      <div className={styles.audioWrapper}>
+        Original Video Audio
+        <VolumeControl volume={originalVideoVolume} setVolume={setOriginalVideoVolume} />
+      </div>
+      {
+        YouTubeMusic.map((audio, i) => {
+          return (
+            <div key={i} className={styles.audioWrapper}>
+              {audio.name}
+              <YouTubeAudioPlayer height={"1vh"} width={"1vw"} videoId={audio.id} volume={audioVolumes ? audioVolumes[i] : 0} />
+            </div>
+          )
+        })
+      }
+    </div>
+  );
+}
+
+export default YouTubeMusicModal;
---
+++ app/src/utils/Music.js
@@ -0,0 +1,10 @@
+const YouTubeMusic = [
+    { id: "p29JUpsOSTE", name: "1" },
+    { id: "HVau-JRGirg", name: "2" },
+    { id: "bzBH9Nm1BP8", name: "3" },
+    { id: "xDWG9SrB4io", name: "4" },
+    { id: "SE9nDvo94hw", name: "5" },
+    { id: "WNcsUNKlAKw", name: "6" },
+];
+
+export { YouTubeMusic }
