911efa7f2e25345cd94b96f84304784579c2c637
snowflake64062 <154459241+snowflake64062@users.noreply.github.com>
2024-01-20 21:07:44 +0000

    audio files v1

--- app/src/utils/Themes.js
+++ app/src/utils/Themes.js
@@ -1,5 +1,5 @@
 const AllThemes = [
-  { id: "MYPVQccHhAQ", img: "cafe.png", name: "Cafe 1", category: ["Default", "Cafe"] },
+  { id: "MYPVQccHhAQ", img: "cafe.png", name: "Cafe 1", category: ["Cafe", "Default"] },
   { id: "0L38Z9hIi5s", img: "cafe2.png", name: "Cafe 2", category: ["Default", "Cafe"] },
   { id: "fX7k3W-2BMM", img: "ghibli.png", name: "Ghibli", category: ["Default", "Anime"] },
   { id: "HGl75kurxok", img: "ghibli.png", name: "Anime", category: ["Default", "Anime"] },
@@ -7,6 +7,7 @@ const AllThemes = [
   { id: "jfKfPfyJRdk", img: "ani1.png", name: "Lofi Stream", category: ["Default", "Lofi"]},
   { id: "4vIQON2fDWM", img: "library2.png", name: "Library", category: ["Default", "Library"] },
   { id: "YQc4WT0yDH4", img: "library.png", name: "Bookstore", category: ["Default", "Library"] },
+  { id: "UGRWG6wxXAw", img: "rain.png", name: "Rain", category: ["Default", "Rain"]}
 ];
 
 const AllCategories = [
@@ -14,6 +15,8 @@ const AllCategories = [
   "Rain:1",
   "Anime:2",
   "Lofi:3",
+  "Nature:4",
+  "Others:5",
 ]
 AllCategories.sort((a, b) => {return a.localeCompare(b)}); //sort
 
