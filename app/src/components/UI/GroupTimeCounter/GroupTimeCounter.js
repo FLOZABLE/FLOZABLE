0035d64fa13bf6d3dd6c66803ce1bab67cf78d6a
snowflake64062 <154459241+snowflake64062@users.noreply.github.com>
2024-01-22 20:36:00 +0000

    time counter calcuation fix 2

--- app/src/components/UI/GroupTimeCounter/GroupTimeCounter.js
+++ app/src/components/UI/GroupTimeCounter/GroupTimeCounter.js
@@ -44,7 +44,7 @@ function GroupTimeCounter({
 
     return (
         <div className={styles.GroupTimeCounter}>
-            {Math.round(groupTotal * 100/3060)/100}hr
+            {Math.round(groupTotal * 100/3600)/100}hr
         </div>
     );
 }
