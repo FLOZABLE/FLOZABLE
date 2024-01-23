979ba9d2a3211ab37a5e269b0cd8609a575d4540
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2024-01-22 16:38:55 -0800

    v0.4.0 (beta v5.0)

--- extension/popup.js
+++
@@ -1,6 +0,0 @@
-/* document.addEventListener('DOMContentLoaded', () => {
-  chrome.runtime.sendMessage('getTabUsageData', (response) => {
-    const tabUsageDataElement = document.getElementById('tabUsageData');
-    tabUsageDataElement.textContent = response;
-  });
-}); */
