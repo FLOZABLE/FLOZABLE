c385cd803b6c48374eb874180d7024b75e889a7b
HackingGo306 <121586285+HackingGo306@users.noreply.github.com>
2024-01-30 18:57:22 +0000

    theme preview draggable

--- app/src/components/UI/ThemePreview/ThemePreview.js
+++ app/src/components/UI/ThemePreview/ThemePreview.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styles from "./ThemePreview.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -8,12 +8,14 @@ import {
 import YouTubePlayer from "../YouTubePlayer/YouTubePlayer";
 import BlobBtn from "../BlobBtn/BlobBtn"
 import ThemeCategoryBtn from "../ThemeCategoryBtn/ThemeCategoryBtn";
-import Draggable from "react-draggable";
+import Draggable, {DraggableCore} from "react-draggable";
+
 
 function ThemePreview({ isActive, setIsActive, setResponse }) {
   const [isDragging, setIsDragging] = useState(false);
   const [volume, setVolume] = useState(0);
   const [themeCategory, setThemeCategory] = useState(-1);
+  const draggableRef = useRef(null);
 
   const eventControl = (event) => {
     if (event.type === 'mousemove' || event.type === 'touchmove') {
@@ -31,11 +33,10 @@ function ThemePreview({ isActive, setIsActive, setResponse }) {
       <div className={isActive ? styles.active : styles.hidden}>
         <div className={styles.transitionCircle}>
           <Draggable
-            onDrag={eventControl}
-            onStop={eventControl}
+            nodeRef={draggableRef}
           >
-            <div className={`${styles.controlButtons} ${!isDragging ? styles.initial : ''}`}>
-              <button onClick={() => { if (!isDragging) { setIsActive(prev => !prev) } }}>
+            <div ref = {draggableRef} className={`${styles.controlButtons}`}>
+              <button onClick={() => { setIsActive(prev => !prev) }}>
                 Close
               </button>
               <button onClick={() => { setVolume((volume + 50) % 100) }}>
