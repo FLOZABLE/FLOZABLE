3cc6a80a6fe85a804ea3f893b16503c0d3fb4907
shan jiang <22camerjian@gmail.com>
2024-02-06 19:55:29 -0800

    youtube api v1

---
+++ app/src/components/UI/YoutubeAPI.js
@@ -0,0 +1,45 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import { useGoogleLogin } from "@react-oauth/google";
+
+const serverOrigin = process.env.REACT_APP_ORIGIN;
+
+function YoutubeAPI({ }) {
+
+    const function2 = () => {
+        fetch(`${serverOrigin}/account/auth/youtube`, {
+            method: 'POST',
+        });
+    }
+
+    const login = useGoogleLogin({
+        flow: 'auth-code',
+        select_account: true,
+        onSuccess: (response) => {
+            console.log(response);
+            const { code } = response;
+            fetch(`${serverOrigin}/account/auth/youtube`, {
+                method: "post",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ data: code }),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data);
+                })
+                .catch((error) => console.error(error));
+        },
+        scope: "https://www.googleapis.com/auth/youtube.force-ssl"
+    });
+
+
+    return (
+        <div>
+            <button onClick={login}>Click Me!</button>
+        </div>
+    );
+}
+
+export default YoutubeAPI;
