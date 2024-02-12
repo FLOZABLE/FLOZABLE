16a166e57d7e94044b0d05382ae49d134c4a3031
dIwnsgml <junjason1126@gmail.com>
2024-02-12 14:17:01 -0800

    new subject manager v0

--- app/src/components/UI/SubjectToolManager/SubjectToolManager.js
+++ app/src/components/UI/SubjectToolManager/SubjectToolManager.js
@@ -8,112 +8,111 @@ const serverOrigin = process.env.REACT_APP_ORIGIN;
 
 function SubjectToolManager({ subject, isSubjectToolManager, setIsSubjectToolManager, setResponse }) {
 
-    const [savedTools, setSavedTools] = useState([]);
-    const [otherTools, setOtherTools] = useState([]);
+  const [savedTools, setSavedTools] = useState([]);
+  const [otherTools, setOtherTools] = useState([]);
 
-    useEffect(() => {
-        if (!subject.tools && subject.tools !== "") return;
+  useEffect(() => {
+    if (!subject.tools && subject.tools !== "") return;
 
-        const subjectTools = subject.tools.split(",");
-        let availableTools = [...AllTools];
-        const tempTools = [];
+    const subjectTools = subject.tools.split(",");
+    let availableTools = [...AllTools];
+    const tempTools = [];
 
-        if (subjectTools.length && !!subjectTools[0]) {
+    if (subjectTools.length && !!subjectTools[0]) {
 
-            subjectTools.map((currentTool) => {
-                tempTools.push(AllTools[parseInt(currentTool)]);
-                availableTools = availableTools.filter((tool) => tool.split(",")[0] !== currentTool);
-            });
+      subjectTools.map((currentTool) => {
+        tempTools.push(AllTools[parseInt(currentTool)]);
+        availableTools = availableTools.filter((tool) => tool.split(",")[0] !== currentTool);
+      });
+    }
+
+    setOtherTools(availableTools);
+    setSavedTools(tempTools);
+  }, [subject]);
+
+  const addTool = (toolName) => {
+    if (!toolName) return;
+    const toolIndex = toolName.split(",")[0];
+    setOtherTools(otherTools.filter((tool) => !tool.startsWith(toolIndex)));
+    setSavedTools([...savedTools, AllTools[parseInt(toolIndex)]]);
+  }
+
+  const removeTool = (toolName) => {
+    if (!toolName) return;
+    const toolIndex = toolName.split(",")[0];
+    setSavedTools(savedTools.filter((tool) => !tool.startsWith(toolIndex)));
+    setOtherTools([...otherTools, AllTools[parseInt(toolIndex)]]);
+  }
+
+  const submit = () => {
+    let subjectToolString = "";
+    savedTools.map((tool) => {
+      subjectToolString += tool.split(",")[0] + ",";
+    })
+    subjectToolString = subjectToolString.substring(0, subjectToolString.length - 1);
+
+    console.log(subjectToolString);
+
+    fetch(`${serverOrigin}/study/modify-subject-tools`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        tools: subjectToolString,
+        id: subject.id,
+        name: subject.name,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setResponse(data);
+        if (data.success) {
+          setIsSubjectToolManager(false);
+        };
+      })
+      .catch((error) => console.error(error));
+  }
+
+  return (
+    <div className={styles.SubjectToolManager}>
+      <div className={isSubjectToolManager ? styles.open : styles.hidden}>
+        {
+          savedTools.map((tool, i) => {
+            return (
+              <li
+                className={styles.removeTool}
+                onClick={() => { removeTool(tool) }}
+                key={i}
+              >
+                - {tool.split(",")[1]}
+              </li>
+            );
+          })
         }
-
-        setOtherTools(availableTools);
-        setSavedTools(tempTools);
-    }, [subject]);
-
-    const addTool = (toolName) => {
-        if (!toolName) return;
-        const toolIndex = toolName.split(",")[0];
-        setOtherTools(otherTools.filter((tool) => !tool.startsWith(toolIndex)));
-        setSavedTools([...savedTools, AllTools[parseInt(toolIndex)]]);
-    }
-
-    const removeTool = (toolName) => {
-        if (!toolName) return;
-        const toolIndex = toolName.split(",")[0];
-        setSavedTools(savedTools.filter((tool) => !tool.startsWith(toolIndex)));
-        setOtherTools([...otherTools, AllTools[parseInt(toolIndex)]]);
-    }
-
-    const submit = () => {
-
-        let subjectToolString = "";
-        savedTools.map((tool) => {
-            subjectToolString += tool.split(",")[0] + ",";
-        })
-        subjectToolString = subjectToolString.substring(0, subjectToolString.length - 1);
-
-        console.log(subjectToolString);
-
-        fetch(`${serverOrigin}/study/modify-subject-tools`, {
-            method: "post",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                tools: subjectToolString,
-                id: subject.id,
-                name: subject.name,
-            }),
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                setResponse(data);
-                if (data.success) {
-                    setIsSubjectToolManager(false);
-                };
-            })
-            .catch((error) => console.error(error));
-    }
-
-    return (
-        <div className={styles.SubjectToolManager}>
-            <div className={isSubjectToolManager ? styles.open : styles.hidden}>
-                {
-                    savedTools.map((tool, i) => {
-                        return (
-                            <li
-                                className={styles.removeTool}
-                                onClick={() => { removeTool(tool) }}
-                                key={i}
-                            >
-                                - {tool.split(",")[1]}
-                            </li>
-                        );
-                    })
-                }
-                {
-                    savedTools.length > 0 ?
-                        <hr></hr>
-                        :
-                        <div />
-                }
-                {
-                    otherTools.map((tool, i) => {
-                        return (
-                            <li
-                                className={styles.addTool}
-                                onClick={() => { addTool(tool) }}
-                                key={i}
-                            >
-                                + {tool.split(",")[1]}
-                            </li>
-                        );
-                    })
-                }
-                <BlobBtn delay={-1} name="Save" setClicked={submit} />
-            </div>
-        </div>
-    );
+        {
+          savedTools.length > 0 ?
+            <hr></hr>
+            :
+            <div />
+        }
+        {
+          otherTools.map((tool, i) => {
+            return (
+              <li
+                className={styles.addTool}
+                onClick={() => { addTool(tool) }}
+                key={i}
+              >
+                + {tool.split(",")[1]}
+              </li>
+            );
+          })
+        }
+        <BlobBtn delay={-1} name="Save" setClicked={submit} />
+      </div>
+    </div>
+  );
 }
 
 export default SubjectToolManager;
--- app/src/components/UI/SubjectsManager/SubjectsManager.js
+++ app/src/components/UI/SubjectsManager/SubjectsManager.js
@@ -5,26 +5,65 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
 import SubjectManager from "../SubjectManager/SubjectManager";
 import EditSubjectModal from "../EditSubjectModal/EditSubjectModal";
-import { WritePen, Book, Microscope, Article, Coding, Globe, Workout, Alert } from "../../../utils/svgs";
+
+const serverOrigin = process.env.REACT_APP_ORIGIN;
 
 function SubjectsManager({ subjects, setSubjects, setResponse }) {
   const [isEditSubjectModal, setIsEditSubjectModal] = useState(false);
-  const [selectedSubject, setSelectedSubject] = useState(null);
+  const [selectedSubject, setSelectedSubject] = useState({
+    submit: false,
+    color: null,
+    icon: null,
+    name: null,
+    id: null,
+  });
+
+  useEffect(() => {
+    if (!selectedSubject || !selectedSubject.submit) return;
+
+    const { id, icon, color, name } = selectedSubject;
+    console.log(selectedSubject)
+    fetch(`${serverOrigin}/study/modify-subject`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        color,
+        icon,
+        id,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setResponse(data);
+        if (data.success) {
+          /* let newState = [...subjects];
+          newState = newState.filter((subject) => subject.id != data.subjectInfo.id);
+          newState.push({ ...data.subjectInfo });
+          setSubjects(newState); */
+          //clear new subject info from modal
+        };
+        setSelectedSubject(prev => ({...prev, submit: false}))
+      })
+      .catch((error) => console.error(error));
+  }, [selectedSubject]);
 
   return (
     <div className={styles.SubjectsManager}>
-        <EditSubjectModal
-          subject={selectedSubject}
-          subjects={subjects}
-          setSubjects={setSubjects}
-          setResponse={setResponse}
-          isEditSubjectModal={isEditSubjectModal}
-          setIsEditSubjectModal={setIsEditSubjectModal}
-        />
+      {/* <EditSubjectModal
+        subject={selectedSubject}
+        subjects={subjects}
+        setSubjects={setSubjects}
+        setResponse={setResponse}
+        isEditSubjectModal={isEditSubjectModal}
+        setIsEditSubjectModal={setIsEditSubjectModal}
+      /> */}
       {
         subjects.map((subject, i) => {
           return (
-            <SubjectManager key={i} subject={subject} setSelectedSubject={setSelectedSubject} setIsEditSubjectModal={setIsEditSubjectModal} setResponse={setResponse} />
+            <SubjectManager key={i} subject={subject} setSelectedSubject={setSelectedSubject} selectedSubject={selectedSubject} />
           )
         })
       }
