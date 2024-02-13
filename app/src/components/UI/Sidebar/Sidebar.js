f8b46928dbf4e9030e1d4388cec501cd31ca66ab
dIwnsgml <junjason1126@gmail.com>
2024-02-13 11:51:02 -0800

    new ui v0

--- app/src/components/UI/Sidebar/Sidebar.js
+++ app/src/components/UI/Sidebar/Sidebar.js
@@ -14,7 +14,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import styles from "./Sidebar.module.css";
-import { Knife } from "../../../utils/svgs";
+import { IconBxHome, Knife } from "../../../utils/svgs";
 
 function Sidebar({
   onMouseEnter,
@@ -23,172 +23,61 @@ function Sidebar({
   isSidebarOpen,
   mode,
 }) {
-  const [hoveredElement, setHoveredElement] = useState(null);
-
-  const handleMouseEnter = (elementId) => {
-    setHoveredElement(elementId);
-  };
-
-  const handleMouseLeave = () => {
-    setHoveredElement("null");
-  };
   return (
-    <aside
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-      className={`${styles.Sidebar} ${mode === "study" ? styles.studyMode : ""
-        } ${isSidebarOpen ? styles.isOpen : ""} ${isSidebarHovered ? styles.isOpen : isSidebarHovered
-        }`}
-    >
-      {isSidebarOpen}
-      <div
-        onMouseEnter={() => handleMouseEnter("navItem1")}
-        onMouseLeave={handleMouseLeave}
-        className={`${styles.startStudy} ${styles.navItemWrapper} ${hoveredElement === "navItem1" ? styles.hovered : ""
-          }`}
-      >
-        <Link to="/dashboard" className={styles.navItem}>
-          <div className={styles.icon}>
-            <FontAwesomeIcon icon={faHouse} />
-          </div>
-          <p className={styles.navText} data-text="Home">
-            Home
-          </p>
+    <aside className={styles.Sidebar}>
+      <div className={styles.logo}>
+        <img src="/logo.png" alt="" />
+      </div>
+      <div className={styles.sidebarContainer}>
+        <Link
+          className={styles.sidebarEl}
+        >
+          <i>
+            <IconBxHome /> 
+          </i>
+          <h1>Home</h1>
+        </Link>
+        <Link
+          className={styles.sidebarEl}
+        >
+          <i>
+            <IconBxHome /> 
+          </i>
+          <h1>Home</h1>
+        </Link>
+        <Link
+          className={styles.sidebarEl}
+        >
+          <i>
+            <IconBxHome /> 
+          </i>
+          <h1>Home</h1>
+        </Link>
+        <Link
+          className={styles.sidebarEl}
+        >
+          <i>
+            <IconBxHome /> 
+          </i>
+          <h1>Home</h1>
+        </Link>
+        <Link
+          className={styles.sidebarEl}
+        >
+          <i>
+            <IconBxHome /> 
+          </i>
+          <h1>Home</h1>
+        </Link>
+        <Link
+          className={styles.sidebarEl}
+        >
+          <i>
+            <IconBxHome /> 
+          </i>
+          <h1>Home</h1>
         </Link>
       </div>
-      <ul>
-        <li>
-          <Link
-            className={styles.navItemWrapper}
-            to="/dashboard/stats"
-          >
-            <div className={styles.navItem}>
-              <div className={styles.icon}>
-                <FontAwesomeIcon icon={faChartColumn} />
-              </div>
-              <p className={styles.navText} data-text="Stats">
-                Stats
-              </p>
-            </div>
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={styles.navItemWrapper}
-            to="/dashboard/planner"
-          >
-            <div className={styles.navItem}>
-              <div className={styles.icon}>
-                <FontAwesomeIcon icon={faCalendar} />
-              </div>
-              <p className={styles.navText} data-text="Planner">
-                Planner
-              </p>
-            </div>
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={styles.navItemWrapper}
-            to="/dashboard/groups"
-          >
-            <div className={styles.navItem}>
-              <div className={styles.icon}>
-                <FontAwesomeIcon icon={faPeopleGroup} />
-              </div>
-              <p className={styles.navText} data-text="Groups">
-                Groups
-              </p>
-            </div>
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={styles.navItemWrapper}
-            to="/dashboard/ranking"
-          >
-            <div className={styles.navItem}>
-              <div className={styles.icon}>
-                <FontAwesomeIcon icon={faRankingStar} />
-              </div>
-              <p className={styles.navText} data-text="Ranking">
-                Ranking
-              </p>
-            </div>
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={styles.navItemWrapper}
-            to="/dashboard/friends"
-          >
-            <div className={styles.navItem}>
-              <div className={styles.icon}>
-                <FontAwesomeIcon icon={faUserGroup} />
-              </div>
-              <p className={styles.navText} data-text="Friends">Friends</p>
-            </div>
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={styles.navItemWrapper}
-            to="/dashboard/themes"
-          >
-            <div className={styles.navItem}>
-              <div className={styles.icon}>
-                <FontAwesomeIcon icon={faShop} />
-              </div>
-              <p className={styles.navText} data-text="Themes">
-                Themes
-              </p>
-            </div>
-          </Link>
-        </li>
-        <li>
-          <div className={styles.studyOption}>
-            <Link
-              className={styles.navItemWrapper}
-              to="/dashboard/study"
-            >
-              <div className={styles.navItem}>
-                <div className={styles.icon}>
-                  <FontAwesomeIcon icon={faPencil} />
-                </div>
-                <p className={styles.navText} data-text="Study">
-                  Study
-                </p>
-              </div>
-            </Link>
-          </div>
-        </li>
-        {/* <li>
-          <div
-            className={styles.navItemWrapper}
-          >
-            <Link to="/dashboard/challenges" className={styles.navItem}>
-              <div className={styles.icon}>
-              <FontAwesomeIcon icon={faHandFist} />
-              </div>
-              <p className={styles.navText} data-text="Compete">
-                Compete
-              </p>
-            </Link>
-          </div>
-        </li> */}
-        {/* <li>
-        <div
-            className={styles.navItemWrapper}
-          >
-            <Link to="/dashboard/friends" className={styles.navItem}>
-              <div className={styles.icon}>
-                <img src={<Knife />} alt="" />
-              </div>
-              <p className={styles.navText} data-text="Friends">Friends</p>
-            </Link>
-          </div>
-        </li> */}
-      </ul>
     </aside>
   );
 }
