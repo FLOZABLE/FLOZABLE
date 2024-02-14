96665982d1241d2f3c612d9757c22fa4c4429393
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2024-02-14 01:41:21 -0800

    rechart v0

--- app/src/components/UI/SmallSubjectsViewer/SmallSubjectsViewer.js
+++ app/src/components/UI/SmallSubjectsViewer/SmallSubjectsViewer.js
@@ -1,16 +1,13 @@
 import styles from "./SmallSubjectsViewer.module.css";
 import ChartDataLabel from "chartjs-plugin-datalabels";
 import plugin from "chartjs-plugin-datalabels";
-import { colorsList } from "../../../constant";
+import { coldColorsList, colorsList, warmColorsList } from "../../../constant";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import PieChart from "../PieChart";
+import { PieChart, Pie, Tooltip, ResponsiveContainer, Label } from "recharts";
+/* 
 
-function SmallSubjectsViewer({ subjects }) {
 
-  return (
-    <div className={styles.SmallSubjectsViewer}>
-      {subjects &&  subjects.daily && subjects.daily.groupedTotal[subjects.daily.groupedTotal.length - 1] ?
         <PieChart
           labels={subjects.map((subject) => subject.name)}
           datasets={[
@@ -45,9 +42,55 @@ function SmallSubjectsViewer({ subjects }) {
           }}
           plugins={ChartDataLabel}
         />
+*/
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+function SmallSubjectsViewer({ subjects }) {
+
+  return (
+    <div className={styles.SmallSubjectsViewer}>
+      {subjects && subjects.daily && subjects.daily.groupedTotal[subjects.daily.groupedTotal.length - 1] ?
+        <div className={styles.chartContainer}>
+          <ResponsiveContainer width="100%" height="100%">
+            <PieChart>
+              <Tooltip />
+              <Pie
+                         cx="50%"
+                         cy="50%"
+                         labelLine={false}
+                data={subjects.reduce((arr, subject) => {
+                  const total = subject.daily.total[subject.daily.total.length - 1];
+                  if (total) {
+                    console.log(arr.length % (warmColorsList.length - 1))
+                    const fill = coldColorsList[arr.length % (coldColorsList.length - 1)];
+                    arr.push({ total, subject, fill });
+                  };
+                  return arr;
+                }, []
+                )}
+                dataKey="total"
+                outerRadius={150}
+                innerRadius={100}
+                fill="green"
+                label={renderCustomizedLabel}
+              >
+              </Pie>
+            </PieChart>
+          </ResponsiveContainer>
+        </div>
         : <div className={styles.noSubjects}>
           <Link to={"/dashboard/study"}>
-          Study to see stats!
+            Study to see stats!
           </Link>
         </div>
       }
