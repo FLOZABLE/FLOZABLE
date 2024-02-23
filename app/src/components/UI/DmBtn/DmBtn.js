73ac1069b6c3372130a778d51bcaf16c923b2f96
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2024-02-23 05:24:21 -0800

    friend search ui + radiak bar filer err fixed

--- app/src/components/UI/DmBtn/DmBtn.js
+++ app/src/components/UI/DmBtn/DmBtn.js
@@ -5,7 +5,7 @@ import { faComments } from "@fortawesome/free-solid-svg-icons";
 
 const serverOrigin = process.env.REACT_APP_ORIGIN;
 
-function DmBtn({userInfo, setResponse}) {
+function DmBtn({userInfo, setResponse, padding}) {
   const requestChat = () => {
     fetch(`${serverOrigin}/chat/chat-request`, {
       method: "post",
@@ -27,7 +27,7 @@ function DmBtn({userInfo, setResponse}) {
   return (
     <div className={styles.DmBtn}>
       <div className={styles.blobWrapper}>
-        <BlobBtn delay={-1} name={<FontAwesomeIcon icon={faComments} />} setClicked={() => { requestChat() }} opt={2} />
+        <BlobBtn padding={padding} name={<FontAwesomeIcon icon={faComments} />} setClicked={() => { requestChat() }} opt={2} />
       </div>
       <div className={styles.hoverEl}>
         <p>Chat with {userInfo ? userInfo.name : ''}!</p>
--- app/src/components/UI/SearchBar/SearchBar.js
+++ app/src/components/UI/SearchBar/SearchBar.js
@@ -1,18 +1,24 @@
 import { IconSearch } from "../../../utils/svgs";
 import styles from "./SearchBar.module.css";
 
-function SearchBar({ searchQuery, setSearchQuery }) {
+function SearchBar({ searchQuery, setSearchQuery, onEnter = () => { } }) {
   return (
     <div className={styles.SearchBar}>
       <div className={styles.inputContainer}>
-        <input type="text" value={searchQuery} onChange={(e) => {setSearchQuery(e.target.value)}} />
+        <input type="text" value={searchQuery} onChange={(e) => { setSearchQuery(e.target.value) }} onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            onEnter();
+          }
+        }} />
         <div className={`${styles.circles} ${searchQuery.length ? styles.hidden : ''}`}>
-        <div className={styles.circle}></div>
-        <div className={styles.circle}></div>
-        <div className={styles.circle}></div>
+          <div className={styles.circle}></div>
+          <div className={styles.circle}></div>
+          <div className={styles.circle}></div>
+        </div>
       </div>
-      </div>
-      <i>
+      <i onClick={() => {
+        onEnter();
+      }}>
         <IconSearch />
       </i>
     </div>
