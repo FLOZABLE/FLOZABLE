450634a0147b46fe31b22bfbd7696cc219170412
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2024-02-25 03:32:41 -0800

    responsive dashboard

--- app/src/components/UI/RefreshBtn/RefreshBtn.js
+++ app/src/components/UI/RefreshBtn/RefreshBtn.js
@@ -3,11 +3,11 @@ import styles from "./RefreshBtn.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRotate } from "@fortawesome/free-solid-svg-icons";
 
-function RefreshBtn({refresh, setRefresh}) {
+function RefreshBtn({ refresh, setRefresh }) {
   const [lastUpd, setLastUpd] = useState(new Date().getTime() / 1000);
 
   return (
-    <div className={styles.RefreshBtn}
+    <i className={`${styles.RefreshBtn} ${refresh ? styles.refresh : ''}`}
       onClick={() => {
         const now = new Date().getTime() / 1000;
         if (now - lastUpd > 3) {
@@ -19,11 +19,8 @@ function RefreshBtn({refresh, setRefresh}) {
         };
       }}
     >
-      <i className={refresh ? styles.refresh : ''}>
-        <FontAwesomeIcon icon={faRotate}
-        />
-      </i>
-    </div>
+      <FontAwesomeIcon icon={faRotate} />
+    </i>
   )
 };
 
