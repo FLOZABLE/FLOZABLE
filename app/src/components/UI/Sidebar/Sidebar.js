374bbef68e663772aaa17fa996cf633cefaf9dbd
TK <david12banana72@gmail.com>
2024-02-28 22:23:20 +0900

    Sidebar Animation Update

--- app/src/components/UI/Sidebar/Sidebar.js
+++ app/src/components/UI/Sidebar/Sidebar.js
@@ -5,7 +5,7 @@ import {
   faHouse,
   faCalendar,
   faUserGroup,
-  faRankingStar,
+  faRankingStar, 
   faPeopleGroup,
   faShop,
   faHandFist,
@@ -31,70 +31,107 @@ function Sidebar({
         </a>
       </div>
       <div className={styles.sidebarContainer}>
-        <Link
-          className={styles.sidebarEl}
-          to={"/dashboard"}
-        >
-          <i>
-            <IconBxHome />    
-          </i>
-          <h1>Home</h1>
-        </Link>
-        <Link
-          className={styles.sidebarEl}
-          to={"/dashboard/stats"}
-        >
-          <i>
-            <IconMonitor /> 
-          </i>
-          <h1>Stats</h1>
-        </Link>
-        <Link
-          className={styles.sidebarEl}
-          to={"/dashboard/planner"}
-        >
-          <i>
-            <IconClipboardOutline /> 
-          </i>
-          <h1>Planner</h1>
-        </Link>
-        <Link
-          className={styles.sidebarEl}
-          to={"/dashboard/ranking"}
-        >
-          <i>
-            <IconStatsChart /> 
-          </i>
-          <h1>Rank</h1>
-        </Link>
-        <Link
-          className={styles.sidebarEl}
-          to={"/dashboard/groups"}
-        >
-          <i>
-            <IconPeople16 /> 
-          </i>
-          <h1>Groups</h1>
-        </Link>
-        <Link
-          className={styles.sidebarEl}
-          to={"/dashboard/friends"}
-        >
-          <i>
-            <IconUserAdd /> 
-          </i>
-          <h1>Friends</h1>
-        </Link>
-        <Link
-          className={styles.sidebarEl}
-          to={"/dashboard/themes"}
-        >
-          <i>
-            <IconGalleryLine /> 
-          </i>
-          <h1>Themes</h1>
-        </Link>
+      <div>
+        <div>
+           <Link
+             className={styles.sidebarEl}
+             to={"/dashboard"}
+           >
+              <div className={styles.hoverField}>
+                <h4>Dashboard</h4>
+              </div>
+             <i>
+               <IconBxHome />    
+             </i>
+             {/* <h1>Home</h1> */}
+           </Link> 
+          </div>
+        <div>
+          <Link
+            className={styles.sidebarEl}
+            to={"/dashboard/stats"}
+          >
+            <div className={styles.hoverField}>
+              <h4>Stats</h4>
+            </div>
+            <i>
+              <IconMonitor /> 
+            </i>
+            {/* <h1>Stats</h1> */}
+          </Link>
+        </div>
+        <div>
+          <Link
+            className={styles.sidebarEl}
+            to={"/dashboard/planner"}
+          >
+            <div className={styles.hoverField}>
+              <h4>Planner</h4>
+            </div>
+            <i>
+              <IconClipboardOutline /> 
+            </i>
+           {/* <h1>Planner</h1> */}
+          </Link>
+        </div>
+        <div>
+          <Link
+            className={styles.sidebarEl}
+            to={"/dashboard/ranking"}
+          >
+            <div className={styles.hoverField}>
+              <h4>Rank</h4>
+            </div>
+            <i>
+              <IconStatsChart /> 
+            </i>
+          {/* <h1>Rank</h1> */}
+          </Link>
+        </div>
+        <div>
+          <Link
+            className={styles.sidebarEl}
+            to={"/dashboard/groups"}
+          >
+            <div className={styles.hoverField}>
+             <h4>Groups</h4>
+           </div>
+            <i>
+             <IconPeople16 /> 
+           </i>
+            {/* <h1>Groups</h1> */}
+         </Link>
+        </div>
+        <div>
+          <Link
+            className={styles.sidebarEl}
+            to={"/dashboard/friends"}
+          >
+            <div className={styles.hoverField}>
+              <h4>Friends</h4>
+           </div>
+            <i>
+              <IconUserAdd /> 
+            </i>
+           {/* <h1>Friends</h1> */}
+          </Link>
+        </div>
+        <div>
+          <Link
+            className={styles.sidebarEl}
+            to={"/dashboard/themes"}
+          >
+            <div className={styles.hoverField}>
+              <h4>Themes</h4>
+            </div>
+            <i>
+              <IconGalleryLine /> 
+            </i>
+            {/* <h1>Themes</h1> */}
+          </Link>
+        </div>
       </div>
+    </div>
     </aside>
   );
 }
