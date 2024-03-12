183f13f4b5b574e904e91673ba325782d0f19435
HackingGo306 <121586285+HackingGo306@users.noreply.github.com>
2024-03-12 19:09:03 +0000

    theme category undefined fix

--- app/src/components/UI/ThemeCategoryBtn/ThemeCategoryBtn.js
+++ app/src/components/UI/ThemeCategoryBtn/ThemeCategoryBtn.js
@@ -20,7 +20,7 @@ AllCategories.map((string) => {
   */
 });
 
-function ThemeCategoryBtn({ themeId, setResponse, themeCategory,  bgColor='#ffffffC0', color='#000' }) {
+function ThemeCategoryBtn({ themeId, setResponse, themeCategory, bgColor = '#ffffffC0', color = '#000' }) {
   const [category, setCategory] = useState(-2);
   const [disp, setDisp] = useState("Save");
   const [isOpen, setIsOpen] = useState(false);
@@ -62,9 +62,9 @@ function ThemeCategoryBtn({ themeId, setResponse, themeCategory,  bgColor='#ffff
       onBlur={() => {
         setIsOpen(false);
       }}
-      style={{backgroundColor: bgColor, color}}
+      style={{ backgroundColor: bgColor, color }}
     >
-      <p className={styles.categoryDisp} style={{ color}}>{disp}</p>
+      <p className={styles.categoryDisp} style={{ color }}>{disp}</p>
       <ul className={styles.options}>
         {options.map((option, i) => {
           const optionArr = option.split(":");
