d7af0504274d4248f6ba014497e8b7b6cca77475
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2024-03-29 01:08:48 -0700

    groups v1

---
+++ next/src/Components/Groups/MemberEl/MemberEl.js
@@ -0,0 +1,111 @@
+import React, { useEffect, useState } from "react";
+import styles from "./MemberEl.module.css";
+import { Link } from "react-router-dom";
+import { StudyPerson, RestPerson } from "../../../utils/svgs";
+import MemberTimer from "../MemberTimer/MemberTimer";
+import MemberCamDisp from "../MemberCamDisp.js/MemberCamDisp";
+import { DateTime } from "luxon";
+import { socket } from "../../../socket";
+import { useContextMenu } from "react-contexify";
+
+function MemberEl({ memberInfo, setStudyingMembers, device, recvTransport }) {
+  const [run, setRun] = useState(false);
+  const [total, setTotal] = useState(0);
+  const [studyIcon, setStudyIcon] = useState(
+    <RestPerson width={"2.5rem"} height={"2.5rem"} opt1={"#fff"} />,
+  );
+
+  const { show } = useContextMenu({
+    id: "ffffff",
+  });
+
+  function handleContextMenu(event) {
+    show({
+      event,
+      props: {
+        key: 'value'
+      }
+    })
+  }
+
+  useEffect(() => {
+    if (!memberInfo) return;
+
+    const { totalTime, activeSubject, user_id } = memberInfo;
+
+    if (activeSubject) {
+      const { id, time } = activeSubject;
+      if (id !== '0') {
+        setRun(true);
+        const liveTotal = parseInt(totalTime) + parseInt(DateTime.now().toSeconds()) - parseInt(time);
+        setTotal(liveTotal);
+        setStudyIcon(
+          <StudyPerson
+            opt1={"#fff"}
+            opt2={"#fff"}
+            width={"2.5rem"}
+            height={"2.5rem"}
+          />
+        )
+      } else {
+        setTotal(parseInt(totalTime));
+      };
+    } else {
+      setTotal(parseInt(totalTime));
+    };
+
+
+    const onStudying = () => {
+      setStudyIcon(
+        <StudyPerson
+          opt1={"#fff"}
+          opt2={"#fff"}
+          width={"2.5rem"}
+          height={"2.5rem"}
+        />
+      );
+      setStudyingMembers(prev => [...prev, memberInfo]);
+      setRun(true);
+    };
+
+    const onStopStudying = () => {
+      setStudyIcon(
+        <RestPerson width={"2.5rem"} height={"2.5rem"} opt1={"#fff"} />
+      );
+      setStudyingMembers(prevMembers => {
+        return prevMembers.filter(member => {
+          return member.user_id !== user_id;
+        });
+      });
+      setRun(false);
+    };
+
+    socket.on(`studying:${user_id}`, onStudying);
+    socket.on(`stopStudying:${user_id}`, onStopStudying);
+
+    return () => {
+      socket.off(`studying:${user_id}`, onStudying);
+      socket.off(`stopStudying:${user_id}`, onStopStudying);
+    };
+  }, [memberInfo]);
+
+  return (
+    <div className={styles.member} onContextMenu={handleContextMenu}>
+      <MemberCamDisp memberInfo={memberInfo} device={device} isFocus={isFocus} recvTransport={recvTransport} isHeadphone={isHeadphone} />
+      <div className={styles.inner}>
+        <Link to={`/dashboard/user/${memberInfo.user_id}`}>
+          <div className={styles.userName}>{memberInfo.name}</div>
+        </Link>
+        <div className={styles.icon}>{studyIcon}</div>
+        <div className={styles.timer}>
+          <MemberTimer
+            initialSec={total}
+            run={run}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MemberEl;
---
+++ next/src/Components/Groups/MyGroupsViewer/MyGroupsViewer.js
@@ -0,0 +1,97 @@
+import React, { useRef, useState, useEffect, useCallback, useContext } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import styles from "./MyGroupsViewer.module.css";
+import MyGroupContainer from "../MyGroupContainer/MyGroupContainer";
+import { CallOptionsContext, GroupsContext, UserInfoContext } from "@/utils/Contexts";
+import { socket } from "@/utils/socket";
+import { mediaSocket } from "@/utils/mediaSocket";
+
+function MyGroupsViewer({
+  myGroups,
+  mode,
+  groupsViewerRef,
+  setIsEditGroupModal,
+}) {
+  const {myGroups} = useContext(GroupsContext);
+  const {userInfo} = useContext(UserInfoContext);
+
+  const {setIsCam, setIsMic} = useContext(CallOptionsContext);
+
+  const [selectedGroupIndex, setSelectedGroupIndex] = useState(0);
+
+  useEffect(() => {
+    mediaSocket.connect();
+  }, []);
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const selectedGroupId = searchParams.get("group");
+    if (!selectedGroupId) return;
+
+    setTimeout(() => {
+      const groupIndex = myGroups.findIndex(group => group.group_id === selectedGroupId);
+      if (groupsViewerRef.current && groupIndex !== -1) {
+        groupsViewerRef.current.swiper.slideTo(groupIndex);
+      };
+    }, 2000);
+  }, [myGroups, groupsViewerRef]);
+
+
+  return (
+    <div
+      className={`${styles.MyGroupsViewer} ${mode === "study" ? styles.study : ""
+        }`}
+    >
+      {myGroups.length ?
+        <Swiper
+          slidesPerView={1}
+          loop={true}
+          pagination={{
+            clickable: true,
+            dynamicBullets: true
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className={styles.Swiper}
+          onSnapIndexChange={(swiperCore) => {
+            const { realIndex } = swiperCore;
+            setSelectedGroupIndex(realIndex);
+            setIsCam(false);
+            setIsMic(false);
+          }}
+          ref={groupsViewerRef}
+        >
+          {myGroups.map((group, i) => {
+            const focus = i === selectedGroupIndex;
+            if (focus) {
+              setTimeout(() => {
+                socket.emit("changeGroup", group.group_id);
+                mediaSocket.emit("changeGroup", group.group_id);
+              }, 500);
+            }
+            return (
+              <SwiperSlide key={i} className={styles.groupsWrapper}>
+                {focus ?
+                  <MyGroupContainer
+                    group={group}
+                    setIsEditGroupModal={setIsEditGroupModal}
+                    mode={mode}
+                    isMine={group.leader === userInfo?.user_id}
+                  /> : null
+                }
+              </SwiperSlide>
+            )
+          })}
+        </Swiper>
+        :
+        <div className={styles.noGroup}>You haven't joined any groups yet!</div>
+      }
+    </div>
+  );
+}
+
+export default MyGroupsViewer;
