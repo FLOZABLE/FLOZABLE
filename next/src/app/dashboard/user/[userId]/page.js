609a3e99095b16308062a71f1f7ae0b4d294e27e
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2024-04-04 17:31:02 -0700

    user page v0

--- next/src/app/dashboard/user/[userId]/page.js
+++ next/src/app/dashboard/user/[userId]/page.js
@@ -1,37 +1,78 @@
-"ues client";
+"use client";
 
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import styles from "./page.module.css";
 import CalendarModal from "@/app/components/Modals/CalendarModal/CalendarModal";
 import RadioBtn from "@/app/components/Buttons/RadioBtn/RadioBtn";
 import FriendRequestBtn from "@/app/components/Buttons/FriendRequestBtn/FriendRequestBtn";
 import StudyTrendChart from "@/app/components/Charts/StudyTrendChart";
+import config from "@/app/utils/config";
+import ProfileImage from "@/app/components/Users/ProfileImage/ProfileImage";
+import DmBtn from "@/app/components/Buttons/DmBtn/DmBtn";
+import CountryViewer from "@/app/components/Others/CountryViewer/CountryViewer";
+import RankingTrend from "@/app/components/Charts/RankingTrendChart";
+import { GroupsContext } from "@/app/utils/Contexts";
+import GroupContainer from "@/app/components/Groups/GroupContainer/GroupContainer";
+import FriendsViewer from "@/app/components/Friends/FriendsViewer/FriendsViewer";
+import { DateTime } from "luxon";
+import { timelineSort } from "@/app/utils/timelineSorting";
 
 function User({ params }) {
   const { userId } = params;
+
+  const { groups } = useContext(GroupsContext);
+
+  const [userInfo, setUserInfo] = useState(null);
+  const [isCalendarOpen, setIsCalendarOpen] = useState(false);
+  const [userSubjects, setUserSubjects] = useState([]);
   const [statsViewer, setStatsViewer] = useState('Daily');
   const [viewDate, setViewDate] = useState(new Date(new Date().setHours(0, 0, 0, 0)));
-  
+  const [userGroups, setUserGroups] = useState([]);
+  const [userFriends, setUserFriends] = useState([]);
+
+  useEffect(() => {
+    if (!userId) return;
+
+    fetch(`${config.server}/account/profile/${userId}`, { method: 'get', credentials: 'include' })
+      .then((response) => response.json())
+      .then((data) => {
+
+        if (data.success) {
+          const { userInfo, subjectsInfo, friendsInfo } = data;
+          setUserInfo(userInfo);
+          const sortedSubject = timelineSort(subjectsInfo);
+          setUserSubjects(sortedSubject);
+          const userGroups = groups.filter(group => {
+            return userInfo.groups.includes(group.group_id);
+          });
+          setUserFriends(friendsInfo)
+          setUserGroups(userGroups);
+        };
+      })
+      .catch((error) => console.error(error));
+  }, [userId]);
+
   return (
     <div>
-      <CalendarModal isCalendarOpen={isCalendarOpen} setIsCalendarOpen={setIsCalendarOpen} updateViewDate={updateViewDate} viewDate={viewDate} />
+      <CalendarModal
+        isCalendarOpen={isCalendarOpen}
+        setIsCalendarOpen={setIsCalendarOpen}
+        updateViewDate={setViewDate}
+        viewDate={viewDate}
+      />
       <div className={`Main`}>
         <div className="title">
           User
         </div>
         <div className={styles.User}>
           <div className={styles.searchOpt}>
-            <RadioBtn items={[{ view: 'Daily', value: 'Daily' }, { view: 'Weekly', value: 'Weekly' }, { view: 'Monthly', value: 'Monthly' }]} changeEvent={updateViewer} defaultViewer={0} />
+            <RadioBtn items={[{ view: 'Daily', value: 'Daily' }, { view: 'Weekly', value: 'Weekly' }, { view: 'Monthly', value: 'Monthly' }]} changeEvent={setStatsViewer} defaultViewer={0} />
           </div>
           <div className={styles.profileCard}>
             <div>
-              <div className={styles.profileImg}
-                style={{
-                  backgroundImage: `url("${serverOrigin}/profile-images/${userInfo ? userInfo.user_id : ''}.jpeg")`, backgroundSize: 'cover',
-                  backgroundPosition: 'center center',
-                  backgroundRepeat: 'no-repeat',
-                }}
-              ></div>
+              <ProfileImage
+                userId={userInfo?.user_id}
+              />
               <div className={`${styles.name} overflowDot`}>
                 {userInfo?.name}
               </div>
@@ -41,7 +82,9 @@ function User({ params }) {
             </div>
             <div>
               <FriendRequestBtn userInfo={userInfo} />
-              <DmBtn setIsChatModal={setIsChatModal} setResponse={setResponse} userInfo={userInfo} />
+              <DmBtn
+                userInfo={userInfo}
+              />
             </div>
             <div className={styles.timezone}>
               Timezone:
@@ -62,7 +105,9 @@ function User({ params }) {
               </div>
               <div className={styles.chartContainer}>
                 <StudyTrendChart
-                  subjectsTrend={subjectsTrend}
+                  viewDate={viewDate}
+                  statsViewer={statsViewer}
+                  subjectsProp={userSubjects}
                 />
               </div>
             </div>
@@ -71,27 +116,11 @@ function User({ params }) {
                 Ranking Trend
               </div>
               <div className={styles.chartContainer}>
-                <ResponsiveContainer width="100%" height="100%">
-                  <LineChart
-                    data={rankingsTrend}
-                    margin={{
-                      top: 5,
-                      right: 30,
-                      left: 20,
-                      bottom: 5,
-                    }}
-                  >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="label" tickFormatter={(data) => {
-                      const dateTime = DateTime.fromISO(data);
-
-                      return dateTime.toFormat('M/d');
-                    }} />
-                    <YAxis reversed={true} />
-                    <Tooltip />
-                    <Line type="monotone" dataKey={"ranking"} stroke="#8884d8" activeDot={{ r: 8 }} />
-                  </LineChart>
-                </ResponsiveContainer>
+                <RankingTrend
+                  viewDate={viewDate}
+                  statsViewer={statsViewer}
+                  userInfoProp={userInfo}
+                />
               </div>
             </div>
             <div className={styles.box} id={styles.groupBox}>
@@ -99,18 +128,16 @@ function User({ params }) {
                 {userInfo ? userInfo.name : ''}'s groups
               </div>
               <div className={`${styles.groupsContainer} customScroll`}>
-                <Groups
-                  groups={userGroups}
-                  myGroups={myGroups}
-                  setMyGroups={setMyGroups}
-                  setOtherGroups={setOtherGroups}
-                  setJoinGroupResponse={setResponse}
-                  setIsGroupPwModal={setIsGroupPwModal}
-                  setJoinTarget={setJoinTarget}
-                  userInfo={myInfo}
-                  queryTags={[]}
-                  type={1}
-                />
+                {
+                  userGroups.map((group, i) => {
+                    return (
+                      <GroupContainer
+                        key={i}
+                        groupInfo={group}
+                      />
+                    )
+                  })
+                }
               </div>
             </div>
             <div className={styles.box} id={styles.friendsBox}>
