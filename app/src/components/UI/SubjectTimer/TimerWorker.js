09fef1148b563eebc623390e65368970ecad9bf9
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2024-04-20 03:34:16 -0700

    removed react

--- app/src/components/UI/SubjectTimer/TimerWorker.js
+++
@@ -1,3 +0,0 @@
-const worker = new Worker('/subjectTimerWorker.js');
-
-export default worker;
--- app/src/mediaSocket.js
+++
@@ -1,5 +0,0 @@
-import { io } from 'socket.io-client';
-
-const mediaSocket = io(process.env.REACT_APP_MEDIASOCKET, { autoConnect: false });
-
-export { mediaSocket };
