0384426126301854eb01fe58a2b1005595b324bf
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2024-04-21 22:48:00 -0700

    img static server

--- next/src/app/utils/config.js
+++ next/src/app/utils/config.js
@@ -2,8 +2,9 @@
 
 const config = {
   server: process.env.NEXT_PUBLIC_SERVER,
+  static_server: process.env.NEXT_PUBLIC_STATIC_SERVER,
   location: process.env.NEXT_APP_LOCATION,
-  spotify_client_id: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID
+  spotify_client_id: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
 };
 
 export default config;
