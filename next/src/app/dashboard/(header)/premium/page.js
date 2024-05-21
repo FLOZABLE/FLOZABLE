4e4b3c079c08490ce2ace5080dd439df021ea094
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2024-05-20 23:03:56 -0700

    premium feature

--- next/src/app/dashboard/(header)/premium/page.js
+++ next/src/app/dashboard/(header)/premium/page.js
@@ -8,6 +8,7 @@ import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { PremiumGold, PremiumPremium } from "@/app/utils/Svg";
 import { premiumFeatures } from "@/app/utils/Constant";
 import BlobBtn from "@/app/components/Buttons/BlobBtn/BlobBtn";
+import RadioBtn from "@/app/components/Buttons/RadioBtn/RadioBtn";
 
 export default function Premium() {
   const [type, setType] = useState(1);
@@ -22,13 +23,13 @@ export default function Premium() {
           </div>
           <div>
             <div className={styles.sliderWrapper}>
-              <SlidingOptBtn
-                options={{
-                  0: `Monthly`,
-                  1: `Yearly`,
-                }}
-                value={type}
-                setValue={setType}
+              <RadioBtn
+                items={[
+                  { view: "Monthly", value: 0 },
+                  { view: "Yearly", value: 1 },
+                ]}
+                changeEvent={setType}
+                defaultViewer={1}
               />
             </div>
             <div className={styles.cardContainer}>
@@ -70,23 +71,32 @@ export default function Premium() {
                   <i>
                     <PremiumPremium />
                   </i>
-                  Premium
+                  <p
+                    className={"jost textGradient"}
+                    style={{
+                      "--primary": "#517dff",
+                      "--secondary": "rgb(66, 186, 255)",
+                    }}
+                  >
+                    Pro
+                  </p>
+                </div>
+                <div className={`${styles.price} jost`}>
+                  <small>$</small>
+                  2.99
                 </div>
                 {premiumFeatures[0].map((feature, i) => {
                   return (
                     <div className={styles.item} key={i}>
+                      <p>{feature}</p>
                       <i>
                         <FontAwesomeIcon icon={faCheck} />
                       </i>
-                      <p>{feature}</p>
                     </div>
                   );
                 })}
-                <div className={styles.item}>
-                  <i>
-                    <FontAwesomeIcon icon={faCheck} />
-                  </i>
-                  <p>something sdfsdfsd </p>
+                <div className={styles.blobWrapper}>
+                  <BlobBtn>Get Started</BlobBtn>
                 </div>
               </div>
             </div>
