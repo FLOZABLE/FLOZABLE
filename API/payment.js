39a76149cc9d8723a2325a6a24d809ab85cf03b3
dIwnsgml <junjason1126@gmail.com>
2024-05-22 18:55:54 -0700

    payment v3

--- API/payment.js
+++ API/payment.js
@@ -4,51 +4,100 @@ const { autoSignin } = require("../tool");
 const { responseCodes } = require("../Constant");
 const { userCache } = require("../services/redisLoader");
 const pool = require("../model/pool");
+const { validateString, validateURL } = require("../validate");
 const Router = express.Router();
 
 Router.get("/client-secret", async (req, res) => {
-  autoSignin(req, res, async(userId) => {
+  autoSignin(req, res, async (userId) => {
     try {
       const userInfo = await userCache(userId);
 
-      if (!userInfo) return res.send(responseCodes['no-user']);
+      if (!userInfo) return res.send(responseCodes["no-user"]);
 
-      const {name, email} = userInfo;
+      const { name, email } = userInfo;
       const customer = await stripe.paymentIntents.create({
         amount: 2000,
-        currency: 'usd',
+        currency: "usd",
         automatic_payment_methods: {
           enabled: true,
         },
       });
 
       console.log(customer, customer.client_secret);
-      res.send({success: true, secret: customer.client_secret});
+      res.send({ success: true, secret: customer.client_secret });
     } catch (err) {
       console.log(err);
     }
-  })
+  });
 });
 
-Router.post("/invoice", async (req, res) => {
-  autoSignin(req, res, async(userId) => {
-    try {
+Router.post("/create-checkout-session", async (req, res) => {
+  const { priceId, success_url, cancel_url } = req.body;
 
-      const connection = pool.promise();
-      const [[userInfo]] = await connection.query(`SELECT stripe_id FROM users WHERE user_id = ?`, [userId]);
+  try {
+    const isValidPriceId = validateString(priceId, "price id");
 
-      if (!userInfo) return res.send(responseCodes['no-user']);
-
-      const invoice = await stripe.invoices.create({
-        customer: userInfo.stripe_id,
-      });
-
-      console.log(invoice)
-    } catch (err) {
-      console.log(err);
+    if (!isValidPriceId.isValid) {
+      return res.send({ success: false, reason: isValidPriceId.reason });
     }
-  })
+
+    const isValidSuccessUrl = validateURL(
+      success_url,
+      true,
+      process.env.SERVER_CORS.split(", ")
+    );
+
+    if (!isValidSuccessUrl.isValid) {
+      return res.send({ success: false, reason: isValidSuccessUrl.reason });
+    }
+
+    const isValidCancelUrl = validateURL(
+      cancel_url,
+      true,
+      process.env.SERVER_CORS.split(", ")
+    );
+
+    if (!isValidCancelUrl.isValid) {
+      return res.send({ success: false, reason: isValidCancelUrl.reason });
+    }
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price: priceId,
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      success_url: success_url,
+      cancel_url: cancel_url,
+    });
+
+    res.send({ succes: true, client_secret: session.id }); // Returning the session ID (client secret) to the frontend
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
+Router.get("/products", async (req, res) => {
+  try {
+    const prices = await stripe.products.list({
+      limit: 10,
+    });
+
+    console.log(prices)
+
+    const information = prices.data.map((price) => {
+      const { id, active, recurring, product } = price;
+      return { id, active, recurring, product };
+    });
+
+    res.send({ success: true, information });
+
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 module.exports = Router;
--- next/src/app/dashboard/payment/page.js
+++ next/src/app/dashboard/payment/page.js
@@ -5,6 +5,8 @@ import styles from "./page.module.css";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import { useStripeClientSecret } from "@/app/Hooks/payments";
+import { useState } from "react";
+import { useSearchParams } from "next/navigation";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE);
 
@@ -13,7 +15,19 @@ const appearance = {
 };
 
 export default function Payment() {
-  const { data } = useStripeClientSecret();
+  const { data } = useStripeClientSecret(priceId);
+
+  const searchParams = useSearchParams();
+
+  const [priceId, setPriceId] = useState(null);
+
+
+  useState(() => {
+    if (!searchParams) return;
+
+    const priceId = searchParams.get('priceId');
+    setPriceId(priceId);
+  }, [searchParams]);
 
   return (
     <div className={styles.Payment}>
