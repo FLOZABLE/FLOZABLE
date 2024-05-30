8103e07e27438bd1b4d721c16881a162218a141b
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2024-05-29 22:26:49 -0700

    edit subject modal function finished

--- next/src/app/components/Modals/SubjectsModal/SubjectsModal.js
+++ next/src/app/components/Modals/SubjectsModal/SubjectsModal.js
@@ -3,13 +3,29 @@
 import { useContext } from "react";
 import styles from "./SubjectsModal.module.css";
 import { ModalsContext } from "@/app/utils/Contexts";
+import SubjectsManager from "../../Account/SubjectsManager/SubjectsManager";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 export default function SubjectsModal() {
-  const {isSubjectsModal, setIsSubjectsModal} = useContext(ModalsContext);
+  const { isSubjectsModal, setIsSubjectsModal } = useContext(ModalsContext);
 
   return (
-    <div className={`${styles.SubjectsModal} modal ${isSubjectsModal ? "open" : ""}`}>
-      
+    <div
+      className={`${styles.SubjectsModal} modal ${
+        isSubjectsModal ? "open" : ""
+      }`}
+    >
+      <div className={styles.header}>
+        <i
+          onClick={() => {
+            setIsSubjectsModal(!isSubjectsModal);
+          }}
+        >
+          <FontAwesomeIcon icon={faXmark} />
+        </i>
+      </div>
+      <SubjectsManager />
     </div>
-  )
+  );
 }
