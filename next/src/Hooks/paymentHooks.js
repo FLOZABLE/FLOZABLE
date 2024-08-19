4be566334ff66b96e97ece6ffce7a7b53f574468
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2024-08-18 22:27:30 -0700

    payment system working + payment page v0 + paymenthook added

---
+++ next/src/Hooks/paymentHooks.js
@@ -0,0 +1,46 @@
+import { useQuery } from "@tanstack/react-query";
+import { UserInfoContext } from "@/app/utils/Contexts";
+import { useContext } from "react";
+import { getProduct, postSubscriptionInitialize } from "@/Api/payment";
+
+function useSubscriptionInitialize(priceId) {
+  const queryResult = useQuery({
+    queryKey: [`useSubscriptionInitialize`],
+    queryFn: () => postSubscriptionInitialize(priceId),
+    staleTime: 1000 * 60 * 10,
+    enabled: !!priceId,
+  });
+
+  const {
+    data: subscriptionInitializeData,
+    isLoading: subscriptionInitializeIsLoading,
+  } = queryResult;
+
+  const clientSecret = subscriptionInitializeData?.clientSecret;
+
+  return {
+    clientSecret,
+    subscriptionInitializeData,
+    subscriptionInitializeIsLoading,
+    ...queryResult,
+  };
+}
+
+function useProduct(priceId) {
+  const queryResult = useQuery({
+    queryKey: [`useProduct`],
+    queryFn: () => getProduct(priceId),
+    staleTime: 1000 * 60 * 10,
+    enabled: !!priceId,
+  });
+
+  const { data: productData, isLoading: productIsLoading } = queryResult;
+
+  return {
+    productData,
+    productIsLoading,
+    ...queryResult,
+  };
+}
+
+export { useSubscriptionInitialize, useProduct };
--- next/src/app/dashboard/payment/page.js
+++ next/src/app/dashboard/payment/page.js
@@ -9,57 +9,82 @@ import {
   useElements,
   useStripe,
 } from "@stripe/react-stripe-js";
-import { useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
-import config from "@/app/utils/config";
+import { useContext, useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useProduct, useSubscriptionInitialize } from "@/Hooks/paymentHooks";
+import { BackArrow } from "@/app/utils/Svg";
+import { ResponseContext } from "@/app/utils/Contexts";
+import BlobBtn from "@/app/components/Buttons/BlobBtn/BlobBtn";
 
 const stripePromise = getStripe();
 
-function PaymentForm({ clientSecret }) {
+function PaymentForm() {
   const stripe = useStripe();
   const elements = useElements();
-  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const { setResponse } = useContext(ResponseContext);
+
+  const router = useRouter();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    setLoading(true);
 
-    if (!stripe || !elements) return;
+    if (!stripe || !elements) {
+      setLoading(false);
+      return;
+    }
 
-    const cardElement = elements.getElement(CardElement);
-
-    const { error, paymentIntent } = await stripe.confirmCardPayment(
-      clientSecret,
-      {
-        payment_method: {
-          card: cardElement,
-        },
-      }
-    );
+    const { error, paymentIntent } = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        // Optionally, you can specify a return_url to redirect after confirmation.
+        // return_url: 'https://your-website.com/order/complete',
+      },
+      redirect: "if_required",
+    });
 
     if (error) {
-      setError(error.message);
+      console.log(error);
+      setResponse({ success: false, reason: error.message });
     } else if (paymentIntent && paymentIntent.status === "succeeded") {
       // Handle successful subscription
+      setResponse({ success: true, msg: "Subscription succeeded!" });
+      router.push("/dashboard");
       console.log("Subscription succeeded!");
     }
+
+    setLoading(false);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <PaymentElement />
-      <button type="submit" disabled={!stripe}>
-        Subscribe
-      </button>
-      {error && <div>{error}</div>}
+    <form onSubmit={handleSubmit} className={styles.PaymentForm}>
+      <div className={styles.PaymentElement}>
+        <PaymentElement />
+      </div>
+      <BlobBtn disabled={!stripe || loading} type={"submit"}>
+        {loading ? "Processing..." : "Subscribe"}
+      </BlobBtn>
     </form>
   );
 }
 
 export default function Payment() {
-  const [priceId, setPriceId] = useState("");
+  const [priceId, setPriceId] = useState(null);
   const [clientSecret, setClientSecret] = useState(null);
+  const [productInfo, setProductInfo] = useState({
+    name: "",
+    price: 0,
+    interval: "",
+  });
+
+  const { subscriptionInitializeData, subscriptionInitializeIsLoading } =
+    useSubscriptionInitialize(priceId);
+  const { productData, productIsLoading } = useProduct(priceId);
 
   const searchParams = useSearchParams();
+  const router = useRouter();
 
   useEffect(() => {
     if (!searchParams) return;
@@ -69,38 +94,54 @@ export default function Payment() {
   }, [searchParams]);
 
   useEffect(() => {
-    console.log("price", priceId);
+    if (!subscriptionInitializeData?.success) return;
 
-    const success_url = window.location.href;
-    const cancel_url = window.location.href;
+    setClientSecret(subscriptionInitializeData.clientSecret);
+  }, [subscriptionInitializeData]);
 
-    if (!priceId) return;
-
-    fetch(`${config.server}/payment/subscription/initialize`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-      body: JSON.stringify({ priceId, success_url, cancel_url }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        if (data.success) {
-          setClientSecret(data.clientSecret);
-        }
-      })
-      .catch((error) => console.error(error));
-  }, [priceId]);
+  useEffect(() => {
+    if (!productData?.success) return;
+    const name = productData.product.name;
+    const interval = productData.price.recurring.interval;
+    const price = productData.price.unit_amount / 100;
+    setProductInfo({
+      name,
+      interval,
+      price,
+    });
+  }, [productData]);
 
   return (
     <div className={styles.Payment}>
-      {clientSecret ? (
-        <Elements stripe={stripePromise} options={{ clientSecret }}>
-          <PaymentForm clientSecret={clientSecret} />
-        </Elements>
-      ) : null}
+      <div className={styles.productInfo}>
+        <div className={styles.contents}>
+          <div className={styles.layer}>
+            <i
+              id={styles.backBtn}
+              onClick={() => {
+                router.back();
+              }}
+            >
+              <BackArrow />
+            </i>
+          </div>
+          <div className={styles.layer} id={styles.name}>
+            <h2>Subscribe to {productInfo.name}</h2>
+          </div>
+          <div className={styles.layer} id={styles.price}>
+            <p>
+              ${productInfo.price} <strong>per {productInfo.interval}</strong>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.paymentInfo}>
+        {clientSecret ? (
+          <Elements stripe={stripePromise} options={{ clientSecret }}>
+            <PaymentForm clientSecret={clientSecret} />
+          </Elements>
+        ) : null}
+      </div>
     </div>
   );
 }
