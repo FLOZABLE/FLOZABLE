26d49461ec477858448dad8da6738ba3e86d5a1e
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2024-08-26 21:34:15 -0700

    groups/themes user dependency removed

--- next/src/Hooks/themesHooks.js
+++ next/src/Hooks/themesHooks.js
@@ -4,13 +4,10 @@ import { UserInfoContext } from "@/app/utils/Contexts";
 import { useContext } from "react";
 
 function useThemes() {
-  const { userInfo } = useContext(UserInfoContext);
-
   const queryResult = useQuery({
     queryKey: [`getThemes`],
     queryFn: getThemes,
-    staleTime: 1000 * 60,
-    enabled: !!userInfo,
+    staleTime: 1000 * 60 * 10,
   });
 
   const { data: useThemesData } = queryResult;
