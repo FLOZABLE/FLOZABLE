3d089a737214fd8f28aa2519a36c042dcb3c0f04
TK <david12banana72@gmail.com>
2024-10-21 21:18:47 +0900

    Header & Sidebar Ui renewal

--- next/src/app/components/Buttons/StudyBtn/StudyBtn.js
+++ next/src/app/components/Buttons/StudyBtn/StudyBtn.js
@@ -4,7 +4,9 @@ import styles from "./StudyBtn.module.css";
 export default function StudyBtn() {
   return (
     <Link href={"/dashboard/study"} className={styles.StudyBtn}>
-      Study
+      <i>
+        Study
+      </i>
     </Link>
   );
 }
