f8416354abd1196c21c440ddd03428046de3dcad
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2024-11-17 15:52:41 -0800

    study heatmap finalzied

--- next/src/app/components/Charts/StudyHeatMap/StudyHeatMap.js
+++ next/src/app/components/Charts/StudyHeatMap/StudyHeatMap.js
@@ -1,32 +1,77 @@
 import styles from "./StudyHeatMap.module.css";
 import { useSubjects } from "@/Hooks/subjectsHooks";
 import CalendarHeatmap from "react-calendar-heatmap";
+import { Tooltip } from "react-tooltip";
 import "react-calendar-heatmap/dist/styles.css";
+import "react-tooltip/dist/react-tooltip.css";
+import { DateTime } from "luxon";
+import { secondConverter } from "@/app/utils/Tool";
+import styled from "@emotion/styled";
 
-export default function StudyHeatMap() {
+const StyleWrapper = styled.div`
+  .react-calendar-heatmap .color-scale-0 {
+    fill: #d6e685;
+  }
+  .react-calendar-heatmap .color-scale-1 {
+    fill: #8cc665;
+  }
+  .react-calendar-heatmap .color-scale-2 {
+    fill: #44a340;
+  }
+  .react-calendar-heatmap .color-scale-3 {
+    fill: #1e6823;
+  }
+
+  .react-calendar-heatmap-weekday-labels {
+    transform: translate(0px, 14px) !important;
+  }
+`;
+export default function StudyHeatMap({ viewDate, setViewDate }) {
   const { groupedSubjects } = useSubjects();
-  console.log(groupedSubjects, "gg");
 
   return (
     <div className={styles.StudyHeatMap}>
-      <CalendarHeatmap
-        startDate={new Date("2016-01-01")}
-        endDate={new Date("2016-04-01")}
-        titleForValue={(value) => "value"}
-        tooltipDataAttrs={(value) => {
-          return {
-            "data-tip": `${value.date} has count: ${value.count}`,
-          };
-        }}
-        values={[
-          { date: "2016-01-01", count: 12 },
-          { date: "2016-01-22", count: 122 },
-          { date: "2016-01-30", count: 38 },
-          // ...and so on
-        ]}
-        showWeekdayLabels={true}
-        onClick={value => alert(`Clicked on value with count: ${value.count}`)}
-      />
+      <StyleWrapper>
+        <CalendarHeatmap
+          startDate={DateTime.fromJSDate(viewDate)
+            .endOf("year")
+            .minus({ year: 1 })
+            .toJSDate()}
+          endDate={DateTime.fromJSDate(viewDate).endOf("year").toJSDate()}
+          /* titleForValue={(value) => "value"} */
+          tooltipDataAttrs={(value) => {
+            if (!value.date) {
+              return null;
+            }
+            const label = secondConverter(value.count).formattedValue;
+            return {
+              "data-tooltip-id": "study-heatmap-tooltip",
+              "data-tooltip-content": `${label} on ${value.date}`,
+              title: "",
+            };
+          }}
+          values={
+            groupedSubjects.day?.total.map((day) => ({
+              date: day.date,
+              count: day.data,
+            })) || []
+          }
+          showWeekdayLabels={true}
+          onClick={(value) => {
+            console.log(value);
+            if (!value.date) return;
+            const date = DateTime.fromISO(value.date).toJSDate();
+            setViewDate(date);
+          }}
+          classForValue={(value) => {
+            if (!value) {
+              return "color-empty";
+            }
+            return `color-scale-${Math.floor(value.count / 3600)}`;
+          }}
+        />
+        <Tooltip id="study-heatmap-tooltip" />
+      </StyleWrapper>
     </div>
   );
 }
