8d7e32ec7447870003732c17edd5fa0aaab7bcc8
dlwnsgml <79344902+dIwnsgml@users.noreply.github.com>
2024-11-26 02:46:55 -0800

    text editor renewal v0

--- next/src/app/components/Inputs/TextEditor/TextEditor.js
+++ next/src/app/components/Inputs/TextEditor/TextEditor.js
@@ -1,48 +1,104 @@
-import React from "react";
-//import ReactQuill from "react-quill";
+import React, { useEffect, useState } from "react";
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
 import styles from "./TextEditor.module.css";
-import "react-quill/dist/quill.snow.css";
-import styled from "@emotion/styled";
-import dynamic from "next/dynamic";
+import DropDownButton from "../../Buttons/DropDownButton/DropDownButton";
+import Document from "@tiptap/extension-document";
+import Paragraph from "@tiptap/extension-paragraph";
+import Text from "@tiptap/extension-text";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBold,
+  faItalic,
+  faListOl,
+  faListUl,
+} from "@fortawesome/free-solid-svg-icons";
 
-const ReactQuill = dynamic(() => import("react-quill"), {
-  ssr: false,
-});
+const MenuOption = ({ onClick, children }) => {
+  return (
+    <button className={styles.MenuOption} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
 
-const StyleWrapper = styled.div`
-  .ql-container {
-    max-height: 15.625rem;
-    overflow-y: auto;
-    border-bottom-right-radius: 0.625rem;
-    border-bottom-left-radius: 0.625rem;
-  }
-  .ql-toolbar {
-    border-top-right-radius: 0.625rem;
-    border-top-left-radius: 0.625rem;
-  }
-  .ql-container::-webkit-scrollbar-track {
-    -webkit-box-shadow: inset 0 0 0.375rem rgba(0, 0, 0, 0.3);
-    border-radius: 0.625rem;
+const MenuBar = ({ editor }) => {
+  const [textMode, setTextMode] = useState(0);
+
+  useEffect(() => {
+    if (!editor) return;
+
+    if (textMode === 0) {
+      editor.chain().focus().setNode("paragraph").run();
+    } else {
+      editor.chain().focus().toggleHeading({ level: textMode }).run();
+    }
+  }, [textMode, editor]);
+
+  if (!editor) {
+    return null;
   }
 
-  .ql-container::-webkit-scrollbar {
-    width: 0.75rem;
-  }
+  return (
+    <div className={styles.MenuBar}>
+      <DropDownButton
+        options={[
+          { name: "Heading 1", value: 1 },
+          { name: "Heading 2", value: 2 },
+          { name: "Heading 3", value: 3 },
+          { name: "Paragraph", value: 0 },
+        ]}
+        setValue={setTextMode}
+        value={textMode}
+      />
+      <MenuOption
+        onClick={() => {
+          editor.chain().focus().toggleItalic().run();
+        }}
+      >
+        <FontAwesomeIcon icon={faItalic} />
+      </MenuOption>
+      <MenuOption
+        onClick={() => {
+          editor.chain().focus().toggleBold().run();
+        }}
+      >
+        <FontAwesomeIcon icon={faBold} />
+      </MenuOption>
+      <MenuOption
+        onClick={() => {
+          editor.chain().focus().toggleOrderedList().run();
+        }}
+      >
+        <FontAwesomeIcon icon={faListOl} />
+      </MenuOption>
+      <MenuOption
+        onClick={() => {
+          editor.chain().focus().toggleBulletList().run();
+        }}
+      >
+        <FontAwesomeIcon icon={faListUl} />
+      </MenuOption>
+    </div>
+  );
+};
 
-  .ql-container::-webkit-scrollbar-thumb {
-    border-radius: 0.625rem;
-    -webkit-box-shadow: inset 0 0 0.375rem rgba(0, 0, 0, 0.3);
-    background-color: #555555;
-  }
-`;
-function TextEditor({ value, setValue }) {
+export default function TextEditor() {
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      /* Document, // Top-level 'doc' node
+      Paragraph, // Default 'p' tag for paragraphs
+      Text, // Basic text node */
+    ],
+    content: "<p>Hello World! 🌎️</p>",
+    immediatelyRender: false,
+  });
+
   return (
     <div className={styles.TextEditor}>
-      <StyleWrapper>
-        {/* <ReactQuill theme="snow" onChange={setValue} value={value} /> */}
-      </StyleWrapper>
+      <MenuBar editor={editor} />
+      <EditorContent editor={editor} className={styles.EditorContent} />
     </div>
   );
 }
-
-export default TextEditor;
