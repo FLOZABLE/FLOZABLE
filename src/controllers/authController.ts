316cb1725b8e20a346949f0005dcbc287cfabc10
dIwnsgml <junjason1126@gmail.com>
2025-05-24 15:34:10 -0700

    create subject & acc service added

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,13 +1,26 @@
 import { Request, Response, NextFunction } from 'express';
 import { SignupRequestBody } from '../types/authTypes';
+import { createUser } from '../services/authService';
+import { createSubject } from '../services/subjectService';
 
-export const signup = (
+export const signup = async (
   req: Request<{}, {}, SignupRequestBody>,
   res: Response,
   next: NextFunction,
 ) => {
   try {
-    const { name, email, password } = req.body;
+    const { name, email, timezone, password } = req.body;
+
+    const newUser = await createUser({ name, email, timezone, password });
+    const newSubject = await createSubject({
+      name: 'others',
+      color: '#000000',
+      users: {
+        connect: { user_id: newUser.user_id },
+      },
+    });
+
+    console.log(newUser, newSubject);
 
     res.send({ success: true });
   } catch (error) {
---
+++ src/types/subjectTypes.ts
@@ -0,0 +1,45 @@
+export type TimeRange = [number, number]; // [start, duration]
+
+export interface TimelineEntry {
+  date: string;
+  data: TimeRange[]; // Array of [start, duration]
+}
+
+export interface TotalEntry {
+  date: string;
+  data: number; // Represents total time/focus in minutes or another unit
+}
+
+export interface FocusEntry {
+  date: string;
+  data: number; // Represents focus time
+}
+
+export interface TimePeriodData {
+  timeline: TimelineEntry[];
+  total: TotalEntry[];
+  focus: FocusEntry[];
+}
+
+export interface Subject {
+  subject_id: string;
+  name: string;
+  color: string;
+  created_at: number;
+  timeline: TimeRange[];
+  day: TimePeriodData;
+  week: TimePeriodData;
+  month: TimePeriodData;
+}
+
+export interface GroupedSubjects {
+  day: TimePeriodData;
+  week: TimePeriodData;
+  month: TimePeriodData;
+}
+
+export interface ActiveSubject {
+  subject_id: string;
+  start_time: number;
+  name: string;
+}
