68cf74ff0f95901a057f6a14473e6723e86b33ef
dIwnsgml <junjason1126@gmail.com>
2025-06-08 13:18:15 -0700

    update ranking service err fixed & get user ranking trend finalized

--- src/routes/rankingRoutes.ts
+++ src/routes/rankingRoutes.ts
@@ -1,11 +1,20 @@
 import { Router } from 'express';
 
-import { getRanking } from '../controllers/rankingController';
+import { getRanking, getUserRanking } from '../controllers/rankingController';
 import { validate } from '../middlewares/validationMiddleWare';
-import { getRankingSchema } from '../schemas/rankingSchemas';
+import {
+  getRankingSchema,
+  getUserRankingSchema,
+} from '../schemas/rankingSchemas';
 
 const rankingRouter = Router();
 
 rankingRouter.get('/', validate(getRankingSchema, 'query'), getRanking);
+
+rankingRouter.get(
+  '/:user_id',
+  validate(getUserRankingSchema, 'query'),
+  getUserRanking,
+);
 
 export default rankingRouter;
