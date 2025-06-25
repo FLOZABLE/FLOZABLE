db7bf4793f9cda7d5881136480c126d538744404
dIwnsgml <junjason1126@gmail.com>
2025-06-24 18:45:09 -0700

    del cached user friends/groups updated & friend request reply finalized

--- src/types/friendTypes.ts
+++ src/types/friendTypes.ts
@@ -1,4 +1,12 @@
-// POST /friend/:friend_Id/request
-export interface PostFriendRequestParams {
+export interface FriendIdParams {
   friend_id: string;
+}
+
+export interface FriendshipIdParams {
+  friendship_id: string;
+}
+
+// POST /friend/:friend_Id/request/reply
+export interface PostFriendRequestReplyBody {
+  accepted: boolean;
 }
