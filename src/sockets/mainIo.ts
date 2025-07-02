5746ae75338fce11114e6e6c85c562b996978954
dIwnsgml <junjason1126@gmail.com>
2025-07-02 14:47:52 +0900

    study service seperated

--- src/sockets/mainIo.ts
+++ src/sockets/mainIo.ts
@@ -12,6 +12,7 @@ import {
   getCachedUserStatus,
 } from '../services/cacheService';
 import { getUserIdByToken } from '../services/sessionService';
+import { handleStudyStart, handleStudyStop } from '../services/studyService';
 import { getIO } from './io';
 
 let mainIo: Namespace | null = null;
@@ -38,105 +39,14 @@ export const registerMainIoEvents = () => {
 
       socket.join(userId);
 
-      const now = nowSec();
-
-      const [groups, friends] = await Promise.all([
-        getCachedUserGroups({ userId }),
-        getCachedUserFriends({ userId }),
-      ]);
-
-      const newStatus = await cacheUserStatus({
-        userId,
-        startTime: now,
-      });
-
-      mainIo?.to([...friends, ...groups, userId]).emit('study:start', {
-        user_id: userId,
-        subject: newStatus,
-      });
-
-      mainIo?.to(userId).emit('mystudy:start', {
-        subject: newStatus,
-      });
+      handleStudyStart(userId);
 
       socket.on('study:start', async (subject_id: string) => {
-        const now = nowSec();
-
-        const [subject, groups, friends] = await Promise.all([
-          prisma.subjects.findFirst({
-            select: { name: true },
-            where: {
-              subject_id: subject_id,
-              user_id: userId,
-            },
-          }),
-          getCachedUserGroups({ userId }),
-          getCachedUserFriends({ userId }),
-        ]);
-
-        if (!subject) return;
-
-        mainIo?.to([...friends, ...groups, userId]).emit('study:start', {
-          user_id: userId,
-          subject: { ...subject, subject_id, start_time: now },
-        });
-
-        mainIo?.to(userId).emit('mystudy:start', {
-          subject: { ...subject, subject_id, start_time: now },
-        });
-
-        cacheUserStatus({
-          userId,
-          subjectId: subject_id,
-          name: subject.name,
-          startTime: now,
-        });
+        handleStudyStart(userId, subject_id);
       });
 
       socket.on('study:stop', async () => {
-        try {
-          const now = nowSec();
-
-          const [status, groups, friends] = await Promise.all([
-            getCachedUserStatus(userId),
-            getCachedUserGroups({ userId }),
-            getCachedUserFriends({ userId }),
-          ]);
-
-          console.log('stop', status);
-
-          if (!status || status.subject_id === '0') return;
-
-          const duration = now - status.start_time;
-
-          const newStatus = await cacheUserStatus({ userId, startTime: now });
-
-          mainIo?.to(userId).emit('mystudy:stop', {
-            stopped_subject_id: status.subject_id,
-            duration,
-          });
-          mainIo?.to([...friends, ...groups, userId]).emit('study:stop', {
-            user_id: userId,
-            status: newStatus,
-            duration,
-          });
-
-          await prisma.subject_timelines.create({
-            data: {
-              subject_id: status.subject_id,
-              start_time: status.start_time,
-              duration,
-            },
-          });
-
-          for (let i = -12; i < 12; i++) {
-            cacheRanking(userId, 'day', i, duration);
-            cacheRanking(userId, 'week', i, duration);
-            cacheRanking(userId, 'month', i, duration);
-          }
-        } catch (err) {
-          console.log(err);
-        }
+        handleStudyStop(userId);
       });
 
       socket.on('disconnect', async (reason) => {
@@ -147,46 +57,9 @@ export const registerMainIoEvents = () => {
           //won't terminate if it's mobile or chrome extension
           if (device === 'mobile' || device === 'chrome-extension') return;
 
-          const now = nowSec();
-
-          const [status, groups, friends] = await Promise.all([
-            getCachedUserStatus(userId),
-            getCachedUserGroups({ userId }),
-            getCachedUserFriends({ userId }),
-          ]);
-
-          const duration = status ? now - status.start_time : 0;
-
           await delCachedUserStatus(userId);
 
-          mainIo?.to([...friends, ...groups, userId]).emit('study:stop', {
-            user_id: userId,
-            status: null,
-            duration,
-          });
-
-          if (status) {
-            mainIo?.to(userId).emit('mystudy:stop', {
-              stopped_subject_id: status.subject_id,
-              duration,
-            });
-
-            if (duration && status.subject_id !== '0') {
-              await prisma.subject_timelines.create({
-                data: {
-                  subject_id: status.subject_id,
-                  start_time: status.start_time,
-                  duration,
-                },
-              });
-
-              for (let i = -12; i < 12; i++) {
-                cacheRanking(userId, 'day', i, duration);
-                cacheRanking(userId, 'week', i, duration);
-                cacheRanking(userId, 'month', i, duration);
-              }
-            }
-          }
+          handleStudyStop(userId, true);
         } catch (err) {
           console.log(err);
         }
