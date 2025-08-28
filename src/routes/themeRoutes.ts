19a761a8a4ccdfd79e9f2d23addd90dbc342c239
dIwnsgml <junjason1126@gmail.com>
2025-08-27 22:50:12 -0400

    theme v1 - theme save/unsave v0

--- src/routes/themeRoutes.ts
+++ src/routes/themeRoutes.ts
@@ -1,19 +1,45 @@
 import { Router } from 'express';
 
-import { getThemeAll, putTheme } from '../controllers/themeController';
+import {
+  getThemeAll,
+  getThemeMine,
+  postThemeSave,
+  postThemeUnsave,
+  putTheme,
+} from '../controllers/themeController';
 import { authMiddleware } from '../middlewares/authMiddleware';
 import { validate } from '../middlewares/validationMiddleWare';
-import { putThemeSchema } from '../schemas/themeSchemas';
+import {
+  postThemeSaveSchema,
+  postThemeUnsaveSchema,
+  putThemeSchema,
+} from '../schemas/themeSchemas';
 
 const themeRouter = Router();
 
 themeRouter.get('/all', getThemeAll);
+
+themeRouter.get('/mine', authMiddleware, getThemeMine);
 
 themeRouter.put(
   '/',
   authMiddleware,
   validate(putThemeSchema, 'body'),
   putTheme,
+);
+
+themeRouter.post(
+  '/save',
+  authMiddleware,
+  validate(postThemeSaveSchema, 'body'),
+  postThemeSave,
+);
+
+themeRouter.post(
+  '/unsave',
+  authMiddleware,
+  validate(postThemeUnsaveSchema, 'body'),
+  postThemeUnsave,
 );
 
 export default themeRouter;
