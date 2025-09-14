b26f786e92d57076dd971a46f42db409daf913be
dIwnsgml <junjason1126@gmail.com>
2025-09-14 17:55:02 -0400

    user api seperated

---
+++ src/types/userTypes.ts
@@ -0,0 +1,11 @@
+export interface UserIdParams {
+  user_id: string;
+}
+
+export interface GetUserProfileQuery {
+  timezone: string;
+}
+
+export interface GetUserStatusQuery {
+  timezone: string;
+}
